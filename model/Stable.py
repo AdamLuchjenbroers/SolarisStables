e14fa4fb3c5a9cca6140fe2b0b256da3cd163133
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2011-06-24 13:25:15 +0000

    Add basic Stable model (incomplete)
    svn path=/; revision=11

---
+++ model/Stable.py
@@ -0,0 +1,9 @@
+# -*- coding: iso-8859-1 -*-
+from django.db import models
+
+class Stable(models.Model):
+  StableName = models.CharField(max_length=200)
+  login = models.CharField(max_length=20)
+
+
+
