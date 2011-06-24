1d0384e00cc2aebeae419b2a278f9b7529994506
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2011-06-24 13:11:47 +0000

    Make deploy script executable when it gets redeployed as well
    svn path=/; revision=10

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ svn export http://192.168.7.50/svn/solaris ./ --force
 chown wwwrun:www * -R
 
 chmod ug+x solaris.wsgi
+chmod ug+x deploy.sh
 
 for file in `find . | grep py$`; do
   chmod ug+x $file
