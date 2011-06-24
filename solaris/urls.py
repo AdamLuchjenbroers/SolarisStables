a78544cd3139789ae778cf7d761840a0f65d48c3
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2011-06-24 14:29:30 +0000

    Release 0: Hello World
    svn path=/; revision=12

--- solaris/urls.py
+++ solaris/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: iso-8859-1 -*-
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
@@ -6,7 +7,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^solaris/', include('solaris.foo.urls')),
+    (r'^$', 'solaris.view.Main.index'),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
