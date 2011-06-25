ed768ba7df7d1efed06468a4538ddfc7a4c6a611
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2011-06-25 12:42:47 +0000

    Add an Order column and a boolean to identify which ones belong in the navigation menu
    svn path=/; revision=31

--- solaris/cms/models.py
+++ solaris/cms/models.py
@@ -5,3 +5,5 @@ class StaticContent(models.Model):
   title = models.CharField(max_length=20)
   url = models.CharField(max_length=20)
   content = models.TextField()
+  toplevel = models.BooleanField()
+  order = models.IntegerField()
