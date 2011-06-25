f67af451f5b42bd85afe6765040f88f4b0b452bb
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2011-06-25 08:40:55 +0000

    Flesh out the model design a little bit more
    svn path=/; revision=16

--- solaris/model/Stable.py
+++ solaris/model/Stable.py
@@ -4,6 +4,7 @@ from django.db import models
 class Stable(models.Model):
   StableName = models.CharField(max_length=200)
   login = models.CharField(max_length=20)
-
+  Reputation = models.IntegerField()
+  
 
 
