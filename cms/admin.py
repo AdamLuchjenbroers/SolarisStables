b8715c3c5f6771dda30b8751227633dcddd21b7e
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2011-06-25 12:09:06 +0000

    Whoops
    svn path=/; revision=28

--- cms/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from solaris.cms import models
-
-admin.site.register(models.StaticContent)
--- stablemanager/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from solaris.stablemanager import models
-
-admin.site.register(models.Mech)
-admin.site.register(models.Stable)
