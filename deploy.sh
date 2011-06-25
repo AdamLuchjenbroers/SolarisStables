80b3e888ce28bf6b1b613f0d67639e278c662990
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2011-06-25 02:22:16 +0000

    Add some basic page graphics
    svn path=/; revision=13

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 
 # Script to perform SVN export and set permissions correctly
 svn export http://192.168.7.50/svn/solaris ./ --force
+svn export http://192.168.7.50/svn/solaris/static /var/www/static/ --force
 
-chown wwwrun:www * -R
+rm -rf static
+chown wwwrun:www * /var/www/static -R
 
 chmod ug+x solaris.wsgi
 chmod ug+x deploy.sh
--- solaris/urls.py
+++ solaris/urls.py
@@ -1,18 +1,19 @@
 # -*- coding: iso-8859-1 -*-
 from django.conf.urls.defaults import *
+from django.conf.urls.static import static
+from django.contrib import admin
+from solaris import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
     (r'^$', 'solaris.view.Main.index'),
+    (r'^admin/', include(admin.site.urls)),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
