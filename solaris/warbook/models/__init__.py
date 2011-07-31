b144232d4b02b56e432592ebf1578cd1acd01a52
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2011-07-31 14:05:11 +0000

    Refinement - use dynamic importing as well
    svn path=/; revision=82

--- solaris/warbook/models/__init__.py
+++ solaris/warbook/models/__init__.py
@@ -1,14 +1,12 @@
 from types import ModuleType
 from django.db.models.base import ModelBase
 from sys import modules
-from solaris.warbook.models import PilotAbilities, TechTree
 
-models = modules[__name__]
 moduleList = ['TechTree', 'PilotAbilities']
 __all__ = []
 
 for moduleName in moduleList:
   fullyQualifiedModuleName = '%s.%s' % (__name__, moduleName)
-  moduleObj = modules[fullyQualifiedModuleName]
+  moduleObj = __import__(fullyQualifiedModuleName)
   
   __all__ += [item for item in dir(moduleObj) if isinstance(getattr(moduleObj, item), ModelBase)]
