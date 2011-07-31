a9bc1e86e3ac4972a6d82973507053eaee9153b0
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2011-07-31 13:56:28 +0000

    Use introspection to build module list instead of hard-coding
    svn path=/; revision=81

--- solaris/warbook/models/__init__.py
+++ solaris/warbook/models/__init__.py
@@ -1,4 +1,14 @@
-from TechTree import *
-from PilotAbilities import *
+from types import ModuleType
+from django.db.models.base import ModelBase
+from sys import modules
+from solaris.warbook.models import PilotAbilities, TechTree
 
-__all__ = [Technology, TechnologyRollModifier, PilotAbility, PilotDiscipline]
+models = modules[__name__]
+moduleList = ['TechTree', 'PilotAbilities']
+__all__ = []
+
+for moduleName in moduleList:
+  fullyQualifiedModuleName = '%s.%s' % (__name__, moduleName)
+  moduleObj = modules[fullyQualifiedModuleName]
+  
+  __all__ += [item for item in dir(moduleObj) if isinstance(getattr(moduleObj, item), ModelBase)]
