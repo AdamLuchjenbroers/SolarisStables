b797e9a6401c4f279de16a9fcedb84e2fb59520d
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2011-08-06 00:35:41 +0000

    Add redirect for pilot skills pages
    svn path=/; revision=95

--- solaris/warbook/urls.py
+++ solaris/warbook/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     (r'^techtree$', 'solaris.cms.views.util_redirect', {'path': '/reference/techtree/'}),
     (r'^techtree/$',  'solaris.warbook.techtree.views.list_technologies', {'selected': '/techtree/'}),
     (r'^techtree/(?P<technology>[^/]+)/$', 'solaris.warbook.techtree.views.display_technology', {'selected': '/techtree/'}),
+    (r'^pilotskills$', 'solaris.cms.views.util_redirect', {'path': '/reference/pilotskills/'}),
     (r'^pilotskills/$', 'solaris.warbook.pilotskill.views.list_disciplines')
 )
