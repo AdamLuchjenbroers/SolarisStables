caf7b9f783adceb40f567fa1f07e4a43d27744a1
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-21 22:07:30 +1000

    Comment out the Mech model entries for now

--- solaris/stablemanager/models.py
+++ solaris/stablemanager/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from math import ceil
 
+'''
 class Mech(models.Model):
     mech_name = models.CharField(max_length=25)
     mech_code = models.CharField(max_length=10)
     value = models.IntegerField()
     tonnage = models.IntegerField()
     move_walk = models.IntegerField()
-  
+ 
     def engine_rating(self):
         return self.move_walk * self.tonnage
   
@@ -16,13 +17,16 @@ class Mech(models.Model):
   
     def move_run(self):
         return ceil(self.move_walk * 1.5)
+'''
   
 class Stable(models.Model):
     StableName = models.CharField(max_length=200)
     login = models.CharField(max_length=20)
     Reputation = models.IntegerField()
-  
+
+'''  
 class StableMech(models.Model):
     owner_stable = models.ForeignKey(Stable)
     mech_type = models.ForeignKey(Mech)
     value = models.IntegerField()
+'''
