5378c4bddf1e940d27e056bd2f0f0a9d7f9ead1e
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-21 21:56:06 +1000

    Cap Pilot Abilities at 6

--- solaris/warbook/pilotskill/admin.py
+++ solaris/warbook/pilotskill/admin.py
@@ -4,6 +4,7 @@ from solaris.warbook.pilotskill import models
 class PilotAbilityInline(admin.StackedInline):
     model = models.PilotAbility 
     extra = 6
+    max_num = 6
   
 class PilotDisciplineAdmin(admin.ModelAdmin):
     inlines = [PilotAbilityInline,]
