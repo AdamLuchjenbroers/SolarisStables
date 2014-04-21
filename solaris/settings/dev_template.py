79e3cbe8d0aab393d6b98fd89175fd1c3382e179
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-21 20:31:02 +1000

    Create a template .py file for local settings

---
+++ solaris/settings/dev_template.py
@@ -0,0 +1,54 @@
+#
+# Import all base environment settings
+#
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+  'default' : {
+    'ENGINE'   : 'django.db.backends.postgresql_psycopg2'
+  , 'NAME'     : 'database'
+  , 'USER'     : 'db user'
+  , 'PASSWORD' : 'db password'
+  , 'HOST'     : 'db IP address'
+  }
+}
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+STATIC_ROOT = '/<source folder>/static/'
+STATIC_URL = '/static/'
+USE_DJANGO_STATIC = True 
+
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'Dump a random, reasonably long string of junk here'
+
+
+GENSHI_TEMPLATE_DIRS = (
+     '/<source folder>/solaris/templates/'                   
+#    '/usr/local/django/solaris/templates/'
+)
+
+TEMPLATE_DIRS = (
+      '/<source folder>/solaris/templates/'                
+#    '/usr/local/django/solaris/templates/'
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
