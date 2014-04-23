1f3051caa35c28456ed163a37465548ee1832de4
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-23 20:09:31 +1000

    Stub implementation of Skills Listing page

--- solaris/warbook/pilotskill/views.py
+++ solaris/warbook/pilotskill/views.py
@@ -11,3 +11,10 @@ def list_disciplines(request):
     
     body = Markup(tmpl_tech.generate(disciplines=disciplines))
     return render_page(body=body, selected='^^^^', request=request) 
+
+def show_discipline(request, discipline=''):
+    #discipline = models.PilotDiscipline.objects.filter(name=discipline)
+    #skills = models.PilotAbility.objects.filter(discipline=discipline[0])
+    
+    body = Markup('<p>TODO: List Discipline Skills Here</p>')
+    return render_page(body=body, selected='^^^^', request=request) 
--- solaris/warbook/urls.py
+++ solaris/warbook/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns
 urlpatterns = patterns('',
     (r'^techtree$', 'solaris.cms.views.util_redirect', {'path': '/reference/techtree/'}),
     (r'^techtree/$',  'solaris.warbook.techtree.views.list_technologies', {'selected': '/techtree/'}),
-    (r'^techtree/(?P<technology>[^/]+)/$', 'solaris.warbook.techtree.views.display_technology', {'selected': '/techtree/'}),
+    (r'^techtree/(?P<technology>[^/]+)/?$', 'solaris.warbook.techtree.views.display_technology', {'selected': '/techtree/'}),
     (r'^pilotskills$', 'solaris.cms.views.util_redirect', {'path': '/reference/pilotskills/'}),
-    (r'^pilotskills/$', 'solaris.warbook.pilotskill.views.list_disciplines')
+    (r'^pilotskills/$', 'solaris.warbook.pilotskill.views.list_disciplines'),
+    (r'^pilotskills/(?P<discipline>[^/]+)/?$', 'solaris.warbook.pilotskill.views.show_discipline')
 )
