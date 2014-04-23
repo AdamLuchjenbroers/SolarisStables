b725d8e0ecf499f0dae657360e3a03cd293a2b0e
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-23 22:29:09 +1000

    Register pilots / mechs in Admin interface for now.

--- solaris/stablemanager/admin.py
+++ solaris/stablemanager/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from solaris.stablemanager import models
+from solaris.stablemanager.assets import models as asset_models
 
 #admin.site.register(models.Mech)
 admin.site.register(models.Stable)
+admin.site.register(asset_models.Mech)
+admin.site.register(asset_models.Pilot)
