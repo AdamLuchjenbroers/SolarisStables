4cd4d71d62b2682eb76089aa15a6cd6df743d3d7
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-23 20:18:18 +1000

    Basic pilot skills listing page

--- solaris/warbook/pilotskill/views.py
+++ solaris/warbook/pilotskill/views.py
@@ -1,20 +1,24 @@
 from django_genshi import loader
 from genshi import Markup
 from solaris.warbook.pilotskill import models
+from django.shortcuts import get_object_or_404
 from solaris.core import render_page
 
 def list_disciplines(request):
     disciplines = models.PilotDiscipline.objects.all()
   
     # Render Technology Detail
-    tmpl_tech = loader.get_template('warbook/pilotskill/pilot_disciplines.tmpl')
+    tmpl_disc = loader.get_template('warbook/pilotskill/pilot_disciplines.tmpl')
     
-    body = Markup(tmpl_tech.generate(disciplines=disciplines))
+    body = Markup(tmpl_disc.generate(disciplines=disciplines))
     return render_page(body=body, selected='^^^^', request=request) 
 
 def show_discipline(request, discipline=''):
-    #discipline = models.PilotDiscipline.objects.filter(name=discipline)
-    #skills = models.PilotAbility.objects.filter(discipline=discipline[0])
     
-    body = Markup('<p>TODO: List Discipline Skills Here</p>')
+    disciplineObject = get_object_or_404(models.PilotDiscipline, urlname=discipline)
+    skills = models.PilotAbility.objects.filter(discipline=disciplineObject)
+    
+    tmpl_disc = loader.get_template('warbook/pilotskill/pilot_abilities.tmpl')
+    
+    body = Markup(tmpl_disc.generate(discipline=disciplineObject,abilities=skills))
     return render_page(body=body, selected='^^^^', request=request) 
