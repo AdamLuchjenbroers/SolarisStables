300e651752ed60a09c70c2b26f30d5fa77d0098e
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-23 21:40:10 +1000

    Add field for linking Stables to their supply contracts (technologies)

--- solaris/stablemanager/models.py
+++ solaris/stablemanager/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from solaris.warbook.techtree.models import Technology
   
 class Stable(models.Model):
-    StableName = models.CharField(max_length=200)
+    stable_name = models.CharField(max_length=200)
     Owner = models.OneToOneField(User)
-    Reputation = models.IntegerField()
+    reputation = models.IntegerField()
+    supply_contract = models.ManyToManyField(Technology)
     
     def __unicode__(self):
         return self.StableName
