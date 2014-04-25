32826ef18a991090328e182d41e1033cdce05784
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-25 22:09:57 +1000

    Add a basic test for the skeleton login page.

--- solaris/stablemanager/tests.py
+++ solaris/stablemanager/tests.py
@@ -5,12 +5,13 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import TestCase, Client
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class URLBasicTests(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+    
+    def test_stableRedirect(self):
+        response = self.client.get('/stable/')
+        self.assertEqual(response.status_code, 302, 'Non-logged in users not redirected away from stable')
