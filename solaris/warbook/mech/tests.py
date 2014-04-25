aaae0b4b964972ebcfbbc9a51c845616ea7bc1e9
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-25 21:41:52 +1000

    Redo mech test cases

--- solaris/warbook/mech/tests.py
+++ solaris/warbook/mech/tests.py
@@ -5,29 +5,29 @@ class MechMathsTest(TestCase):
     test_mech = MechDesign()
     
     def setUp(self):
-        self.test_mech
-        self.test_mech.tonnage = 40
-        self.test_mech.move_walk = 4
+        MechDesign.objects.create(mech_name='Wolverine', mech_code='WVR-7D', tonnage=55, move_walk=5, credit_value=11214457, bv_value=1314 )
+        MechDesign.objects.create(mech_name='Raven', mech_code='RVN-4L', tonnage=35, move_walk=6, credit_value=6001425, bv_value=873 )
         
     
     def test_enginerating(self):
         """
         Tests that the engine 'size' formula is implemented correctly
         """
-        self.assertEqual(self.test_mech.engine_rating(), 160, 'Testing engine rating formula')
+        wolverine = MechDesign.objects.get(mech_name='Wolverine', mech_code='WVR-7D')
+        self.assertEqual(wolverine.engine_rating(), 275, 'Failed Test: Testing engine rating formula')
         
     
     def test_runningMP_Even(self):
         """
         Tests that running MP is calculated correctly
         """
-        self.assertEqual(self.test_mech.move_run(), 6, 'Testing Running MP Formula (even)')
+        raven = MechDesign.objects.get(mech_name='Raven', mech_code='RVN-4L')
+        self.assertEqual(raven.move_run(), 9, 'Failed Test: Testing Running MP Formula (even)')
         
     def test_runningMP_Odd(self):
         """
         Tests that running MP is calculated correctly
         """
-        self.test_mech.move_walk = 3
-        self.assertEqual(self.test_mech.move_run(), 5, 'Testing Running MP Formula (odd)')
-        self.test_mech.move_walk = 4
+        wolverine = MechDesign.objects.get(mech_name='Wolverine', mech_code='WVR-7D')
+        self.assertEqual(wolverine.move_run(), 8, 'Failed Test: Running MP Formula (odd)')
 
