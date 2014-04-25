6f8358ede343de2ef52bb6aa275306940d109ea9
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-25 22:02:44 +1000

    Add some basic test cases for Warbook

--- solaris/warbook/tests.py
+++ solaris/warbook/tests.py
@@ -5,12 +5,20 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import TestCase, Client
 
+class URLBasicTests(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+    
+    def test_techtreeURL(self):
+        response = self.client.get('/reference/techtree/')
+        self.assertEqual(response.status_code, 200, 'Unable to retrieve tech-tree list')
+           
+    
+    def test_pilotskillURL(self):
+        response = self.client.get('/reference/pilotskills/')
+        self.assertEqual(response.status_code, 200, 'Unable to retrieve pilot-skill list')
+        
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
