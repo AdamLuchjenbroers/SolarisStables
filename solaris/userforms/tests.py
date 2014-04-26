90697cb02cecb5d868f35f395c85fdc63acbe9c0
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-26 13:30:28 +1000

    Add tests to ensure login error message only displays on a failed login.

--- solaris/userforms/tests.py
+++ solaris/userforms/tests.py
@@ -24,3 +24,18 @@ class LoginRedirectTest(TestCase):
         response = self.client.post('/login/', {'login' : 'login-test', 'pass' : 'pass', 'redirect' : '/stable'})
         self.assertEqual(response.status_code, 302, 'Failed redirect after login (HTTP %s)' % response.status_code)
         self.assertEqual(response.get('Location'), 'http://testserver/stable', 'Redirected to incorrect page: %s ' % response.get('Location') )
+
+
+class LoginErrorTest(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        User.objects.create_user(username='login-test', email='login-test@nowhere.com', password='pass')
+    
+    def test_NoError(self):
+        response = self.client.get('/login/')
+        self.assertNotContains(response, '<span class="login_failed">Invalid Username or Password</span>', 200)
+        
+    def test_loginWithError(self):
+        response = self.client.post('/login/', {'login' : 'login-test', 'pass' : 'wrongpass'})
+        self.assertContains(response, '<span class="login_failed">Invalid Username or Password</span>', 200)
