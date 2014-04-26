d2b212f57de44bd8001ab54f1ec52635cfee9b22
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-26 10:13:27 +1000

    Use a Through table for Pilot Skills so that additional details can be kept stored when needed (e.g. for Weapon Expert)

--- solaris/stablemanager/assets/models.py
+++ solaris/stablemanager/assets/models.py
@@ -3,13 +3,14 @@ from solaris.stablemanager.models import Stable
 from solaris.warbook.mech.models import MechDesign
 from solaris.warbook.pilotskill.models import PilotAbility
 
+
 class Pilot(models.Model):
     stable = models.ForeignKey(Stable, blank=True)
     pilot_name = models.CharField(max_length=50, blank=True)
     pilot_callsign = models.CharField(max_length=20)
     skill_gunnery = models.IntegerField()
     skill_pilotting = models.IntegerField()
-    skill = models.ManyToManyField(PilotAbility, blank=True)
+    skill = models.ManyToManyField(PilotAbility, blank=True, through='PilotTraining')
     
     class Meta:
         verbose_name_plural = 'Pilots'
@@ -18,13 +19,21 @@ class Pilot(models.Model):
         app_label = 'stablemanager'
         
     def __unicode__(self):
-        return self.pilot_callsign    
+        return self.pilot_callsign
     
-
+class PilotTraining(models.Model):
+    pilot = models.ForeignKey(Pilot)
+    training = models.ForeignKey(PilotAbility)
+    notes = models.CharField(max_length=50, blank=True)
+    
+    class Meta:
+        db_table = 'stablemanager_pilottraining'
+        app_label = 'stablemanager'    
+            
 class Mech(models.Model):
-    stable = models.ForeignKey(Stable, blank=True)
+    stable = models.ForeignKey(Stable, blank=True, null=True)
     mech_type = models.ForeignKey(MechDesign)
-    signature_of = models.ForeignKey(Pilot, blank=True)
+    signature_of = models.ForeignKey(Pilot, blank=True, null=True)
     
     class Meta:
         verbose_name_plural = 'Mechs'
