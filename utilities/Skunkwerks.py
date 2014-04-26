466592c732110444bba8a83d0c3af12ab5002de5
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-26 15:21:09 +1000

    Add a Basic SSW file reader (only extracts the header details)

---
+++ utilities/Skunkwerks.py
@@ -0,0 +1,43 @@
+import libxml2
+from math import floor
+
+class SSWFile:
+    def __init__(self, sswFileName=None):
+        self.xmlFile = libxml2.parseFile(sswFileName)
+        
+        self.xpathContext = self.xmlFile.xpathNewContext()
+            
+    def getCost(self):
+        costInfo = self.xpathContext.xpathEval('/mech/cost')
+        return floor( float (costInfo[0].getContent()))        
+    
+    def getBV(self):
+        costInfo = self.xpathContext.xpathEval('/mech/battle_value')
+        return int (costInfo[0].getContent())
+        
+    def getType(self):
+        typeInfo = self.xpathContext.xpathEval('/mech/mech_type')
+        return typeInfo[0].getContent()
+    
+    def getTechBase(self):
+        typeInfo = self.xpathContext.xpathEval('/mech/techbase')
+        return typeInfo[0].getContent()
+    
+    def getTonnage(self):
+        mechInfo = self.xpathContext.xpathEval('/mech/@tons')
+        return int(mechInfo[0].getContent())
+    
+    def getName(self):
+        mechInfo = self.xpathContext.xpathEval('/mech/@name')
+        return mechInfo[0].getContent()
+    
+    def getCode(self):
+        mechInfo = self.xpathContext.xpathEval('/mech/@model')
+        return mechInfo[0].getContent()
+     
+    def getEngineRating(self):
+        mechInfo = self.xpathContext.xpathEval('/mech/engine/@rating')
+        return int(mechInfo[0].getContent())
+
+    def getWalkingMP(self):
+        return floor ( self.getEngineRating() / self.getTonnage())
