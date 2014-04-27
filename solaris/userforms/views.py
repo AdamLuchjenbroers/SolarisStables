3cdbcb22725bddd6931feba7ab5187167f3810c6
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-27 11:08:18 +1000

    Move forms into a separate 'Forms' module

--- solaris/userforms/views.py
+++ solaris/userforms/views.py
@@ -2,9 +2,8 @@
 from genshi import Markup
 from django_genshi import loader
 from solaris.core import render_page
+from .forms import RegistrationForm
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from django.forms import ModelForm, CharField, PasswordInput, ValidationError
 from django.shortcuts import redirect
 
 def login_page(request):
@@ -35,41 +34,6 @@ def login_page(request):
 
     return render_page(body=body, selected=None, request=request)
 
-
-class RegistrationForm(ModelForm):
-    
-    passwordrepeat = CharField(widget=PasswordInput, label='Repeat')
-    password = CharField(widget=PasswordInput)
-    # Eliminate the blurb about valid user names
-    username = CharField()
-    email=CharField(label='E-Mail')
-    
-    def clean(self):
-        super(RegistrationForm,self).clean()
-        
-        if self.cleaned_data["password"] != self.cleaned_data["passwordrepeat"]:
-            print self.cleaned_data
-            raise ValidationError('Passwords entered do not match')
-       
-        return self.cleaned_data
-              
-        
-    def save(self, commit=True):
-        user = super(RegistrationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user  
-    
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'passwordrepeat' )
-        
-    def render(self):
-        form_template = loader.get_template('userforms/register.tmpl')
-
-        return form_template.generate(form_items=Markup(self.as_p()),redirect=None)
-    
 
 def registration_page(request):
     if (request.method == 'POST'):
