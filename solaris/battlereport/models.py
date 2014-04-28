001f95506ba448ada0ad0692072fc159108b5141
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-28 12:32:35 +0000

    Add basic fields for Broadcast Weeks

--- solaris/battlereport/models.py
+++ solaris/battlereport/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
-class BroadcastWeek:
-    pass
+class Zodiac(models.Model):
+    sign = models.CharField(max_length = 20)
+    rules = models.TextField()
 
-class Zodiac:
-    pass
+class BroadcastWeek(models.Model):
+    week_number = models.IntegerField()
+    sign = models.ForeignKeyField(Zodiac)
+    prev_week = models.ForeignKeyField(BroadcastWeek, null=True)
+    next_week = models.ForeignKeyField(BroadcastWeek, null=True)
+    
+    
