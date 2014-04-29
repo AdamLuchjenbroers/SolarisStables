c2a39180819601af6e316bee19aee8421964ea29
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-30 00:13:48 +1000

    Preliminary Stable Registration Form

--- solaris/stablemanager/forms.py
+++ solaris/stablemanager/forms.py
@@ -1,7 +1,10 @@
 from solaris.forms import SolarisForm
 from django.forms import CharField, ModelChoiceField
+from django.db.models import Max
 from solaris.warbook.models import House
 from solaris.warbook.pilotskill.models import PilotDiscipline
+from solaris.stablemanager.models import Stable
+from solaris.battlereport.models import BroadcastWeek
 
 
 class StableRegistrationForm(SolarisForm):
@@ -10,3 +13,27 @@ class StableRegistrationForm(SolarisForm):
     discipline_1 = ModelChoiceField(label='Discipline 1', required=True, queryset=PilotDiscipline.objects.all())
     discipline_2 = ModelChoiceField(label='Discipline 2', required=True, queryset=PilotDiscipline.objects.all())
     
+    def register_stable(self, user):
+        print self.cleaned_data
+        house = House.objects.get(house=self.cleaned_data['house'])
+        
+        stable = Stable.objects.create(
+            stable_name = self.cleaned_data['stable_name']
+        ,   owner = user
+        ,   house = house
+        ,   reputation = 0
+        ,   current_week = self.week
+        )
+                        
+        stable.stable_disciplines.add( PilotDiscipline.objects.get(name=self.cleaned_data['discipline_1']) )
+        stable.stable_disciplines.add( PilotDiscipline.objects.get(name=self.cleaned_data['discipline_2']) )
+        
+        stable.save()    
+    
+    def clean(self):
+        super(StableRegistrationForm,self).clean()
+        
+        aggr = BroadcastWeek.objects.aggregate(Max('week_number'))
+        self.week = BroadcastWeek.objects.get(week_number=aggr['week_number__max'])
+        
+        return self.cleaned_data
--- solaris/stablemanager/views.py
+++ solaris/stablemanager/views.py
@@ -2,10 +2,12 @@ from genshi import Markup
 from django_genshi import loader
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
+from django.http import HttpResponse
+
+
 from solaris.views import SolarisView
 from solaris.core import render_page
-from solaris.stablemanager import models
-from django.http import HttpResponse
+from solaris.stablemanager.models import Stable
 
 
 from .forms import StableRegistrationForm
@@ -13,7 +15,7 @@ from .forms import StableRegistrationForm
 @login_required(login_url='/login')
 def stable_main(request):
     
-    stableList = models.Stable.objects.filter(owner = request.user)
+    stableList = Stable.objects.filter(owner = request.user)
     
     if len(stableList) <> 1:
         return redirect('/stable/register')
@@ -31,6 +33,11 @@ class StableRegistrationView(SolarisView):
         
         self.template = loader.get_template('stablemanager/register.tmpl')
     
+    def create_stable(self, form, request):
+        form.register_stable(request.user)       
+            
+        return HttpResponse(self.in_layout('<p>Valid Form</p>', request))
+    
     def get(self, request):
         form = StableRegistrationForm()
         body = self.template.generate(form_items=Markup(form.as_p()))
@@ -40,8 +47,10 @@ class StableRegistrationView(SolarisView):
     def post(self, request):
         form = StableRegistrationForm(request.POST)
         body = self.template.generate(form_items=Markup(form.as_p()))
-        
-        return HttpResponse( self.in_layout(body, request) )   
+        if form.is_valid():
+            return self.create_stable(form, request)
+        else:       
+            return HttpResponse( self.in_layout(body, request) )   
     #    pass
         
     
