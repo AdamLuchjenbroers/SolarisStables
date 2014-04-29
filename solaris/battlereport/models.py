bb9c37c3b545247745e766a391a4d2eaee6df380
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-29 23:26:34 +1000

    Add __unicode__() implementations and order linking for Zodiac Signs

--- solaris/battlereport/models.py
+++ solaris/battlereport/models.py
@@ -3,10 +3,17 @@ from django.db import models
 class Zodiac(models.Model):
     sign = models.CharField(max_length = 20)
     rules = models.TextField()
+    next = models.ForeignKey('Zodiac', related_name='prev')
+    
+    def __unicode__(self):
+        return self.sign
 
 class BroadcastWeek(models.Model):
     week_number = models.IntegerField()
     sign = models.ForeignKey(Zodiac)
     prev_week = models.ForeignKey('BroadcastWeek', null=True, related_name='next_week')
+    
+    def __unicode__(self):
+        return 'Week %i' % self.week_number
     
     
