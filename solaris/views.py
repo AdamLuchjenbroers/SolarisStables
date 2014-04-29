af7b049b26594edc8f479b94684fe48d501f3f4a
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-29 21:56:18 +1000

    Move rendering into functions to make overriding get() easier

--- solaris/views.py
+++ solaris/views.py
@@ -8,27 +8,26 @@ from genshi import Markup
 
 class SolarisView(View):
     
-    def __init__(self, *args, **kwargs): 
-        self.navigation_options = StaticContent.objects.filter(toplevel=True).order_by('order')
-        
+    def __init__(self, *args, **kwargs):         
         master_template = get_arg('master_template', kwargs, 'layout.tmpl')
-        self.template = loader.get_template(master_template)
+        self.base_layout = loader.get_template(master_template)
         
         self.doctype = get_arg('doctype', kwargs, 'html')        
         self.selected = '/%s' % get_arg('selected', kwargs, '')
         
         self.body_content = get_arg('body', kwargs, Markup('<p>Body Goes Here</p>'))
+        
+    def get_menu(self):
+        return StaticContent.objects.filter(toplevel=True).order_by('order')
     
-    def body(self):
-        return self.body_content
-    
-    def get(self, request):
-        output = self.template.generate(
-            body=self.body()
+    def in_layout(self, body, request):
+        return self.base_layout.generate(
+            body=body
           , selected=self.selected
-          , menu=self.navigation_options
+          , menu=self.get_menu()
           , authenticated = request.user.is_authenticated()
           , username = request.user.username
         ).render(self.doctype, doctype=self.doctype)
-        
-        return HttpResponse(output)
+    
+    def get(self, request):   
+        return HttpResponse(self.in_layout(self.body_content, request))
