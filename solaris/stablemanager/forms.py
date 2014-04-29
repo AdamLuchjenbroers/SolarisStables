50fb00433706b3dfe73ca32457da4b6f16b53757
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-29 23:18:21 +1000

    Convert to SolarisForm, as the view will need to retain most of the applicable logic.

--- solaris/stablemanager/forms.py
+++ solaris/stablemanager/forms.py
@@ -1,8 +1,12 @@
-from solaris.forms import SolarisModelForm
-from .models import Stable
+from solaris.forms import SolarisForm
+from django.forms import CharField, ModelChoiceField
+from solaris.warbook.models import House
+from solaris.warbook.pilotskill.models import PilotDiscipline
 
-class StableRegistrationForm(SolarisModelForm):
-    
-    class Meta:
-        model = Stable
+
+class StableRegistrationForm(SolarisForm):
+    stable_name = CharField(label='Stable Name', required=True)
+    house = ModelChoiceField(label='House', required=True, queryset=House.objects.all())
+    discipline_1 = ModelChoiceField(label='Discipline 1', required=True, queryset=PilotDiscipline.objects.all())
+    discipline_2 = ModelChoiceField(label='Discipline 2', required=True, queryset=PilotDiscipline.objects.all())
     
--- solaris/stablemanager/models.py
+++ solaris/stablemanager/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from solaris.warbook.models import House
 from solaris.warbook.techtree.models import Technology
+from solaris.warbook.pilotskill.models import PilotDiscipline
+from solaris.battlereport.models import BroadcastWeek
   
 class Stable(models.Model):
     stable_name = models.CharField(max_length=200)
     owner = models.OneToOneField(User, null=True)
+    house = models.ForeignKey(House, null=True)
     reputation = models.IntegerField()
     supply_contract = models.ManyToManyField(Technology)
+    stable_disciplines = models.ManyToManyField(PilotDiscipline)
+    current_week = models.ForeignKey(BroadcastWeek, null=True)
     
     def __unicode__(self):
         return self.stable_name
