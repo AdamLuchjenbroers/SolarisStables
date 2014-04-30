33a89d93eae65a28ee2d927d37c1074981a84947
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-04-30 08:39:14 +0000

    Add some basic unit tests for BroadcastWeek

---
+++ solaris/battlereport/tests.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.db.models import Count
+from .models import BroadcastWeek, Zodiac
+'''
+Runs a suite of tests to confirm that the BroadcastWeek model correctly implements the following behavior
+  * Generates a new Broadcast Week if Advance is called and there is no next week present
+  * Returns the next week if advance() is called and there is already a next week
+'''
+class BroadcastWeekTests(TestCase):
+    
+    def setUp(self):
+        z_black = Zodiac.object.create(sign='Black', rules='Test')
+        z_white = Zodiac.object.create(sign='White', rules='Test', next=z_black)
+        z_black.next = z_white
+        
+        self.week_now = BroadcastWeek.objects.create(week_number=2, sign=z_white)
+        self.week_past = BroadcastWeek.objects.create(week_number=1, sign=z_black, next_week=self.week_now)
+        
+    def test_advanceCurrentWeek(self):
+        new_week = self.week_now.advance()
+        
+        check_count = BroadcastWeek.objects.filter(week_number=3).count()
+        
+        self.assertEqual(new_week.week_number, 3, 'Unexpected Week Number: %i' % new_week.week_number)
+        self.assertEqual(check_count, 1, 'Duplicate Weeks found in Database')
+                
+    def test_advanceOldWeek(self):
+        new_week = self.week_past.advance()
+        
+        check_count = BroadcastWeek.objects.filter(week_number=2).count()
+        
+        self.assertEqual(new_week.week_number, 2, 'Unexpected Week Number: %i' % new_week.week_number)
+        self.assertEqual(check_count, 1, 'Duplicate Weeks found in Database')
+        
