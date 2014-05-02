5297263212946f2707e28a2f257e80479cf59c2c
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-02 09:57:53 +0000

    Add some basic decorators for input validation

---
+++ utilities/Validation.py
@@ -0,0 +1,29 @@
+import re
+
+def expect_integer(function):
+    def check(*args, **kwargs):
+        value = function(*args, **kwargs)
+        try:
+            return int(value)
+        except ValueError:
+            return None
+    
+    return check
+
+def expect_regex(function, regex):
+    r_compiled = re.compile(regex)
+    
+    def check(*args, **kwargs):
+        value = function(*args, **kwargs)
+        if r_compiled.match(value):
+            return value
+        else:
+            return None
+    
+    return check  
+    
+def expect_alphastring(function):
+    return expect_regex(function, '^[a-zA-Z ]+$')
+    
+def expect_basicstring(function):
+    return expect_regex(function, '^[a-zA-Z \(\)\-\']+$')
