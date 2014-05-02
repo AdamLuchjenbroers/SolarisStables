65266ef619ecd02e5f36af66c3bfd31a86196b23
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-02 10:41:33 +0000

    Add dictionaries for mapping SSW locations to StableManager location codes

--- utilities/BulkImporter.py
+++ utilities/BulkImporter.py
@@ -12,6 +12,31 @@ sswPattern = re.compile('.*\.ssw$')
 
 from utilities.Skunkwerks import SSWFile
 
+locations_all = {
+	'hd': 'HD',
+	'rt': 'RT',
+	'rtr' : 'RRT',
+	'lt' : 'LT',
+	'ltr' : 'RLT',
+	'ct' : 'CT',
+	'ctr' : 'RCT',
+	'ra' : 'RA',
+	'la' : 'LA',
+	'll' : 'LL',
+	'rl' : 'RL',
+}
+
+locations_biped = locations_all.copy()
+locations_biped['ra'] = 'RA'
+locations_biped['la'] = 'LA'
+locations_biped['ll'] = 'LL'
+locations_biped['rl'] = 'RL'
+
+locations_quad = locations_all.copy()
+locations_quad['ra'] = 'RFL'
+locations_quad['la'] = 'LFL'
+locations_quad['ll'] = 'LRL'
+locations_quad['rl'] = 'RRL'
 
 def recursiveScanAll(path, relative_path='.'):
     
