5e4ce3e8a3646c3631c69fe1dfeaf6d5b21bad85
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-04 23:27:33 +1000

    Move Mech Validation form to solaris.utilities

---
+++ solaris/utilities/forms.py
@@ -0,0 +1,19 @@
+'''
+   ModelForm for validating Mech Data input loaded from SSW files
+   This form is not intended to be rendered to a page
+'''
+
+from django.forms import ModelForm
+
+from solaris.warbook.mech.models import MechDesign
+
+class MechValidationForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        if isinstance(args[0],dict):
+            args[0]['mech_key'] = '%s|%s' % (args[0]['mech_name'].lower(), args[0]['mech_code'].lower())
+        
+        super(MechValidationForm,self).__init__(*args, **kwargs)
+        
+    class Meta:
+        model = MechDesign
+        
--- solaris/warbook/mech/forms.py
+++ solaris/warbook/mech/forms.py
@@ -1,24 +1,7 @@
-'''
-   ModelForm for validating Mech Data input loaded from SSW files
-   This form is not intended to be rendered to a page
-'''
+from django.forms import CharField, IntegerField
 
-from django.forms import ModelForm, CharField, IntegerField
-
-from solaris.warbook.mech.models import MechDesign
 from solaris.forms import SolarisForm
 
-class MechValidationForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        if isinstance(args[0],dict):
-            args[0]['mech_key'] = '%s|%s' % (args[0]['mech_name'].lower(), args[0]['mech_code'].lower())
-        
-        super(MechValidationForm,self).__init__(*args, **kwargs)
-        
-    class Meta:
-        model = MechDesign
-        
-     
 class MechSearchForm(SolarisForm):
     mech_name = CharField(label='Mech Name', required=False)
     mech_code = CharField(label='Mech Code', required=False)
