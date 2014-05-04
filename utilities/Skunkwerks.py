f457e5c534e18bfdffdc591f90d167c75e2d7c9e
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-04 23:02:49 +1000

    Convert Importer to lxml

--- utilities/Skunkwerks.py
+++ utilities/Skunkwerks.py
@@ -1,58 +1,61 @@
-import libxml2
 from math import floor
+from lxml import etree
 
-from Validation import expect_integer, expect_alphastring, expect_basicstring
+from Validation import expect_integer, expect_alphastring
 
 class SSWFile:
     def __init__(self, sswFileName=None):
-        self.xmlFile = libxml2.parseFile(sswFileName)
+        fd = open(sswFileName)
         
-        self.xpathContext = self.xmlFile.xpathNewContext()
+        self.xmlFile = etree.parse(fd)
     
     @expect_integer        
     def getCost(self):
-        costInfo = self.xpathContext.xpathEval('/mech/cost')
-        return floor( float (costInfo[0].getContent()))        
+        costInfo = self.xmlFile.xpath('/mech/cost')
+        return floor( float (costInfo[0].text))        
     
     @expect_integer
     def getBV(self):
         if self.isOmni():
             return 0 # Omnimechs do not store BV values for the base chassis
         else:
-            costInfo = self.xpathContext.xpathEval('/mech/battle_value')
-            return costInfo[0].getContent()
+            costInfo = self.xmlFile.xpath('/mech/battle_value')
+            return costInfo[0].text
         
     @expect_alphastring
     def getType(self):
-        typeInfo = self.xpathContext.xpathEval('/mech/mech_type')
-        return typeInfo[0].getContent()
+        typeInfo = self.xmlFile.xpath('/mech/mech_type')
+        return typeInfo[0].text
     
     @expect_alphastring
     def getTechBase(self):
-        typeInfo = self.xpathContext.xpathEval('/mech/techbase')
-        return typeInfo[0].getContent()
+        typeInfo = self.xmlFile.xpath('/mech/techbase')
+        return typeInfo[0].text
     
     @expect_integer
     def getTonnage(self):
-        mechInfo = self.xpathContext.xpathEval('/mech/@tons')
-        return int(mechInfo[0].getContent())
+        mechInfo = self.xmlFile.xpath('/mech/@tons')
+        return int(mechInfo[0])
     
     def getName(self):
-        mechInfo = self.xpathContext.xpathEval('/mech/@name')
-        return mechInfo[0].getContent()
+        mechInfo = self.xmlFile.xpath('/mech/@name')
+        return mechInfo[0]
     
     def getCode(self):
-        mechInfo = self.xpathContext.xpathEval('/mech/@model')
-        return mechInfo[0].getContent()
+        mechInfo = self.xmlFile.xpath('/mech/@model')
+        return mechInfo[0]
     
     @expect_integer 
     def getEngineRating(self):
-        mechInfo = self.xpathContext.xpathEval('/mech/engine/@rating')
-        return int(mechInfo[0].getContent())
+        mechInfo = self.xmlFile.xpath('/mech/engine/@rating')
+        return int(mechInfo[0])
 
     def getWalkingMP(self):
         return floor ( self.getEngineRating() / self.getTonnage())
     
+    def getArmorType(self):
+        pass
+    
     def isOmni(self): 
-        mechInfo = self.xpathContext.xpathEval('/mech/@omnimech')
-        return mechInfo[0].getContent() == "TRUE"
+        mechInfo = self.xmlFile.xpath('/mech/@omnimech')
+        return mechInfo[0] == "TRUE"
