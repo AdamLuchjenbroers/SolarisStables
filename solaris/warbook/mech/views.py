aeaacef5a98b9be64c7beacb5a35bd8a03e0fc82
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-04 05:02:53 +0000

    Use get_list_or_404 for MechListView

--- solaris/warbook/mech/views.py
+++ solaris/warbook/mech/views.py
@@ -1,7 +1,7 @@
 #from django.http import HttpResponse
 from django_genshi import loader
 from genshi import Markup
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 from django.http import HttpResponse
 
 from solaris.warbook.views import ReferenceView
@@ -9,8 +9,7 @@ from solaris.warbook.mech.models import MechDesign
 
 class MechDetailView(ReferenceView):
     def get(self, request, name='', code=''):
-        
-        mech = MechDesign.objects.get(mech_name=name, mech_code=code)        
+        mech = get_object_or_404(MechDesign, mech_name=name, mech_code=code)        
         
         #tmpl_mech = loader.get_template('warbook/mechs/mech_detail.tmpl')    
         #body = Markup(tmpl_mech.generate(mech=mech))
@@ -20,6 +19,8 @@ class MechDetailView(ReferenceView):
 
 class MechListView(ReferenceView):
     def get(self, request, name=''):
+        mech_list = get_list_or_404(MechDesign, mech_name=Name)
+    
         body = Markup('<p>List of all %s variants to go here</p>' % name)
         
         return HttpResponse(self.in_layout(body, request))
