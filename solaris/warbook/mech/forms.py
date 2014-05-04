d06d976ab3e1b3a4c842cbe22352d67489c18221
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-04 20:57:44 +1000

    Derive mech_key on load.

--- solaris/warbook/mech/forms.py
+++ solaris/warbook/mech/forms.py
@@ -5,10 +5,15 @@
 
 from django.forms import ModelForm
 
-from solaris.warbook.models import MechDesign
+from solaris.warbook.mech.models import MechDesign
 
 class MechValidationForm(ModelForm):
-
+    def __init__(self, *args, **kwargs):
+        if isinstance(args[0],dict):
+            args[0]['mech_key'] = '%s|%s' % (args[0]['mech_name'].lower(), args[0]['mech_code'].lower())
+        
+        super(MechValidationForm,self).__init__(*args, **kwargs)
+        
     class Meta:
         model = MechDesign
         
