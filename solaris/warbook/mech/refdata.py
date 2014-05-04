64ebf47af65cdac03451b4bafd43676b6a40412e
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-05 00:11:49 +1000

    Add Internal Structure by tonnage

--- solaris/warbook/mech/refdata.py
+++ solaris/warbook/mech/refdata.py
@@ -30,6 +30,26 @@ locations_quad = locations_allmechs + locations_quadonly
 
 locations_all = locations_allmechs + locations_quadonly + locations_bipedonly
 
+structure = {
+  20  : { 'head' : 3, 'arm' :  3, 'leg' :  4, 'sidetorso' :  5, 'centertorso' :  6},
+  25  : { 'head' : 3, 'arm' :  4, 'leg' :  6, 'sidetorso' :  6, 'centertorso' :  8},
+  30  : { 'head' : 3, 'arm' :  5, 'leg' :  7, 'sidetorso' :  7, 'centertorso' : 10},
+  35  : { 'head' : 3, 'arm' :  6, 'leg' :  8, 'sidetorso' :  8, 'centertorso' : 11},
+  40  : { 'head' : 3, 'arm' :  6, 'leg' : 10, 'sidetorso' : 10, 'centertorso' : 12},
+  45  : { 'head' : 3, 'arm' :  7, 'leg' : 11, 'sidetorso' : 11, 'centertorso' : 14},
+  50  : { 'head' : 3, 'arm' :  8, 'leg' : 12, 'sidetorso' : 12, 'centertorso' : 16},
+  55  : { 'head' : 3, 'arm' :  9, 'leg' : 13, 'sidetorso' : 13, 'centertorso' : 18},
+  60  : { 'head' : 3, 'arm' : 10, 'leg' : 14, 'sidetorso' : 14, 'centertorso' : 20},
+  65  : { 'head' : 3, 'arm' : 10, 'leg' : 15, 'sidetorso' : 15, 'centertorso' : 21},
+  70  : { 'head' : 3, 'arm' : 11, 'leg' : 15, 'sidetorso' : 15, 'centertorso' : 22},
+  75  : { 'head' : 3, 'arm' : 12, 'leg' : 16, 'sidetorso' : 16, 'centertorso' : 23},
+  80  : { 'head' : 3, 'arm' : 13, 'leg' : 17, 'sidetorso' : 17, 'centertorso' : 25},
+  85  : { 'head' : 3, 'arm' : 14, 'leg' : 18, 'sidetorso' : 18, 'centertorso' : 27},
+  90  : { 'head' : 3, 'arm' : 15, 'leg' : 19, 'sidetorso' : 19, 'centertorso' : 29},
+  95  : { 'head' : 3, 'arm' : 16, 'leg' : 20, 'sidetorso' : 20, 'centertorso' : 30},
+  100 : { 'head' : 3, 'arm' : 17, 'leg' : 21, 'sidetorso' : 21, 'centertorso' : 31},
+}
+
 def is_leg(location):
     return location in ('LL', 'RL', 'RFL', 'LFL', 'RRL', 'LRL')
 
