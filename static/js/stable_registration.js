e68e6c13b0378b0f87d2cfcfc39916a84d0fc7f0
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-09 00:14:14 +1000

    Dynamically update Disciplines based on current user selection

--- static/js/stable_registration.js
+++ static/js/stable_registration.js
@@ -1,44 +1,135 @@
-$( document ).ready(function() {
-  select_list = $('#id_discipline_1 option');
+function SelectList(selector) {
+  this.src_html = $(selector).clone();
   
-  house_disciplines = [];
+  options = [];
   
-  select_list.each( function(i, item) {
-     console.log( $( this ).attr('value') + ": " + $( this ).text() );
+  src_html.each( function(i, item) {	  
+	  options.push( {
+		  element : item,
+		  id : item.value,
+		  text : item.text,
+		  visible : true,
+	  });
   });
   
-  function update_disciplines() {
-    console.log('update_disciplines()');
+  this.hide = function( entry ) {
+     $.each(this.options, function(i, option) {
+    	 if ((entry == option['id']) || (entry == option['text'])) {
+    		 option['visible'] = false;
+    	 };
+     });
+  };
+  
+  this.reset = function() {
+	  $.each(this.options, function(i, option) {
+		option['visible'] = true;
+	  }); 
+  }
+  
+  this.render = function(selected) {
+	  var output = '';
+	  console.log('render(' + selected + ')');
+	  
+	  $.each(this.options, function(i, option) {
+		  option['element'].selected = (
+		    (selected == option['id']) || (selected == option['text'])
+		  );
+		  
+		  if(option['visible'] || option['element'].selected) {
+			  output += '<option value=\"' + option['id'] + '\"';
+			  if (option['element'].selected) {
+				  output += ' selected=\"selected\"';
+			  }
+			  output += '>' + option['text'] + '</option>\n';
+		  }
+	  });
+	  
+	  return output;
+  };
+  
+  return this;
+};
+
+$( document ).ready(function() {
+  var select_list = SelectList('#id_discipline_1 option');  
+  var house_disciplines = [];
+  
+  console.log(select_list);
+  
+  function get_selected_disciplines() {
     selected = [];
-    
+  
     $.each( ['#id_discipline_1', '#id_discipline_2'], function(i, id) {
        val = $(id + ' option:selected');
          
        if (val.attr('value') != '') {
-         selected.push( {
+         selected[id] = {
            field : id,
            id : val.attr('value'),
            title : val.text()
-         });
+         };
        };                 
      });
+     
+     return selected;
+  }
+  
+  /*
+   If Discipline 2 has a value and Discipline 1
+   is blank, move the selection from Discipline 2
+   up to Discipline 1
+  */
+  function rotate_selected_disciplines() {
+    if ($('#id_discipline_1 option:selected').attr('value') != '') {
+      console.log('V:[' + $('#id_discipline_1 option:selected').attr('value') + ']')
+      return;
+    }
     
-    console.log('  selected list built');
+    disc_id = $('#id_discipline_2 option:selected').attr('value');
+    
+    if ( disc_id != '') {
+      $('#id_discipline_1').val(disc_id);
+      $('#id_discipline_2').val('');
+    };    
+  }
+  
+  function update_disciplines() {
+    console.log('update_disciplines()');
+    
+    selected = get_selected_disciplines();
+    
+    select_list.reset();
      
     $.each( house_disciplines, function(i, item) {
-      console.log('Update HD: ' + item['name']);
-                   
-      $.each(selected, function(j,option) {
-        if(selected[j]['id'] == item['id']) {
-           $(selected[j]['field']).val('');
-           selected.splice(j);
-        }
-      });     
-    });    
-       
-     $.each(selected, function(i, d) {
-       console.log(d['title'] + '[' + d['id'] + '] in field ' + d['field']);
-     });
+      select_list.hide(item['id']);
+      
+      $.each( ['#id_discipline_1', '#id_discipline_2'], function(j,id) {
+    	  val = $(id + ' option:selected');
+    	  
+    	  if (val.attr('value') == item['id']) {
+    		  $(id).prop('selectedIndex', 0);
+    	  };
+      });       
+    });  
+    
+    // Move up the disciplines so empty entries are at the bottom of the list;
+    rotate_selected_disciplines();
+    
+    $.each( ['#id_discipline_1', '#id_discipline_2'], function(i, id) {
+        val = $(id + ' option:selected');
+         
+        console.log(id + ' Selected:' + val.attr('value'));
+        console.log(val);
+        
+        if (val.attr('value') != '') {
+          select_list.hide(val.attr('value'));
+        };
+        
+        $(id).html(
+        	select_list.render(val.attr('value'))
+        )
+    });
+     
   }
   
   $('#id_house').on('change', function() {         
@@ -64,4 +155,7 @@ $( document ).ready(function() {
      });
      
   });
+  
+$('#id_discipline_1').on('change', update_disciplines);
+$('#id_discipline_2').on('change', update_disciplines);
 });
