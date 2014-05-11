2d6201aa4213d0d37b5d4568b32888b44ad009f5
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-11 13:46:49 +1000

    Fixed version of manage.py that selects the correct settings file based on environment variables.

---
+++ manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.7
+from django.core.management import execute_manager
+from os import environ
+from importlib import import_module
+
+try:
+    settings = import_module(environ['DJANGO_SETTINGS_MODULE'])
+except ImportError:
+    import sys
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.exit(1)
+
+if __name__ == "__main__":
+    execute_manager(settings) 
