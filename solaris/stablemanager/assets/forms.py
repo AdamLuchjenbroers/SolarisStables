2ab17b45ed022e82c50de3eba0425fbb31549068
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-13 23:35:50 +1000

    Add stub forms for PilotTraining

--- solaris/stablemanager/assets/forms.py
+++ solaris/stablemanager/assets/forms.py
@@ -1,10 +1,20 @@
-from solaris.forms import SolarisModelForm
+from django.forms.models import BaseInlineFormSet, inlineformset_factory
 
-from .models import Pilot
+from solaris.forms import SolarisModelForm, SolarisInlineForm
+
+from .models import Pilot, PilotTraining
 
 class PilotForm(SolarisModelForm):
-    
 
     class Meta:
         model = Pilot
         fields = ('pilot_name', 'pilot_callsign', 'pilot_rank', 'skill_gunnery', 'skill_pilotting', 'exp_character_points')
+        
+class PilotTrainingForm(SolarisInlineForm):
+    
+    class Meta:
+        model = PilotTraining
+        
+class PilotInlineSkillsForm(BaseInlineFormSet):
+    form = PilotTrainingForm
+    model = PilotTraining
