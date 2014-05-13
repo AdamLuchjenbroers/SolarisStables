86d7e899048bb25770956332bec1f7891d4de4b5
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-13 21:04:14 +1000

    Add hidden ID field to LedgerItem forms

--- solaris/stablemanager/ledger/forms.py
+++ solaris/stablemanager/ledger/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, HiddenInput
+from django.forms import ModelForm, HiddenInput, IntegerField
 
 from solaris.forms import SolarisFixedFormMixin
 from solaris.stablemanager.ledger.models import LedgerItem
@@ -7,10 +7,20 @@ class LedgerItemForm(SolarisFixedFormMixin, ModelForm):
     
     inner_form_template = 'stablemanager/ledger_item_add.tmpl'
     
+    id = IntegerField(widget=HiddenInput, required=False)
+    
     def __init__(self, *args, **kwargs):
         super(LedgerItemForm, self).__init__(*args, **kwargs)
                 
         self.fields['type'].widget = HiddenInput()
+        
+        self.fields['id'].initial = self.instance.pk
+        if self.instance.pk:
+            self.submit_action = 'Edit'
+        else:
+            self.submit_action = 'Add'
+        
+        print self.fields
         
         
     def set_tabs(self, form_tab):
@@ -21,7 +31,7 @@ class LedgerItemForm(SolarisFixedFormMixin, ModelForm):
     def as_p(self):
         return self.template.generate(
              fields = self.getAllFields()
-        ,    submit = 'Add'
+        ,    submit = self.submit_action
         ,    post_url = '/stable/ledger'
         ,    submit_tab = self.submit_tab
         )
