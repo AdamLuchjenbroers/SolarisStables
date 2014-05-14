d9552435af4371bb9dc52818156be3a223aae0be
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-14 18:44:15 +1000

    Add transaction handling to Parser

--- solaris/utilities/loader.py
+++ solaris/utilities/loader.py
@@ -6,6 +6,7 @@ from django.db import transaction
 from .parser import SSWMech
 from .forms import MechValidationForm
 
+@transaction.commit_manually
 def parse(sswfile):
     filename = '%s/%s' % (settings.SSW_STOCK_MECHS_ROOT, sswfile)
     fd = open(filename,'rb')
@@ -16,3 +17,6 @@ def parse(sswfile):
     mech_form = MechValidationForm(mech)
     if mech_form.is_valid():
         mech_form.save()
+        transaction.commit()
+    else:
+        transaction.rollback()
