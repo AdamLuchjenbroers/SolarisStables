c7a7ef91260d53be37e19a2279875a992197eea0
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-17 19:15:50 +1000

    Implement parsing for Heatsinks / Jumpjets

--- solaris/utilities/equipment.py
+++ solaris/utilities/equipment.py
@@ -1,5 +1,7 @@
 from copy import deepcopy
 
+from solaris.warbook.equipment.models import Equipment
+
 class CriticalsList(object):
     def __init__(self, criticals=None):
         if criticals and isinstance(criticals, list):
@@ -41,6 +43,9 @@ class CriticalsList(object):
         except TypeError:
             self.add(other)
         return self
+    
+    def __len__(self):
+        return len(self.slots)
         
     def __add__(self, other):
         result = deepcopy(self)
@@ -70,8 +75,208 @@ class SSWItemMounting(dict):
     def add_slot(self, new_slots):
         self['slots'] += new_slots
         
+    def size(self):
+        return len(self['slots'])
+        
     def extrapolate(self, count):
         start = self['slots'].singleslot()
         
         if start:
             self['slots'].add(start, count)
+            
+class SSWMountedItem(dict):
+    
+    default_type = '?'   
+    default_extrapolated = False
+    
+    def __init__(self, eq_class=None):
+        self['mech'] = None
+        self.criticals = None
+        self.mountings = {}
+            
+        if eq_class == None:
+            eq_class = self.__class__.default_type
+                
+        if self['ssw_name']:
+            (self.equipment , created) = Equipment.objects.get_or_create(ssw_name=self['ssw_name'])
+            
+            self['equipment'] = self.equipment.id
+            if created:
+                self.equipment.name = self['name']
+                self.equipment.equipment_class = eq_class
+                self.equipment.save()
+            
+            if self.equipment.critical_func:
+                self.criticals = self.equipment.criticals()
+    
+    def total_mountings(self):
+        return sum(self.mountings[k].size() for k in self.mountings.keys())         
+
+    def mount(self, xmlnode, rear_firing=False, turret=False):
+        #Most entries only record a single slot, and require that we 
+        #extrapolate from there.
+        self.extrapolated = self.__class__.default_extrapolated
+        
+        locations = xmlnode.xpath('./location|./splitlocation')        
+        
+        for loc in locations:
+            if loc.tag == 'location':
+                index = [int(loc.get('index'))+1]
+            if loc.tag == 'splitlocation':
+                count = int(loc.get('number'))
+                start = int(loc.get('index'))
+                index = range(start+1, count+start+1)
+            
+            loc_code = loc.text.lower()
+            
+            if loc.text in self.mountings:
+                self.mountings[loc_code].add_slot(index)
+            else:
+                self.mountings[loc_code] = SSWItemMounting(loc_code, index, rear=rear_firing, turret=turret)
+                
+        if self.total_mountings() > 1:
+            self.extrapolated = True
+        elif self.total_mountings() == 0:
+            self.mountings['--'] = SSWItemMounting('--', [])
+            self.extrapolated = True #Has no slots
+            
+        if self.criticals and not self.extrapolated:
+            self.extrapolate(self.criticals)
+    
+    def extrapolate(self, criticals):
+        if self.extrapolated:
+            return
+        
+        if len(self.mountings) == 1: 
+            loc = self.mountings.keys()[0]
+            self.mountings[loc].extrapolate(criticals)
+            
+        self.extrapolated = True
+        
+class SSWEquipment(SSWMountedItem):
+    
+    default_type = 'Q'
+    
+    def __init__(self, xmlnode):
+        (self['name'], rear, turret) = self.parse_name(xmlnode.xpath['./name[1]'].text)
+        self.node_type = xmlnode.tag
+        self['ssw_name'] = '%s - %s' % (self.node_type, self.name)
+        
+        self.mount(xmlnode, rear, turret)
+        super(SSWEquipment,self).__init__()
+        
+    def parse_name(self, equipment_name):
+        rear = False
+        turret = False
+        
+        if equipment_name[0:3] == '(T)':
+            turret = True
+            equipment_name = equipment_name[4:]
+            
+        if equipment_name[0:3] == '(R)':
+            rear = True
+            equipment_name = equipment_name[4:]
+            
+        return (equipment_name, rear, turret)
+        
+class SSWStructure(SSWMountedItem):
+    default_type = 'S'
+    default_extrapolated = True
+
+    def __init__(self, xmlnode):
+        
+        structure_type = xmlnode.xpath('./type/text()')[0]
+        self['ssw_name'] = 'Structure - %s' % structure_type
+        self['name'] = structure_type
+        
+        super(SSWStructure,self).__init__()        
+        self.mount(xmlnode, False, False)
+ 
+class SSWArmour(SSWMountedItem):
+    
+    default_type = 'S'
+    
+    def __init__(self, xmlnode):
+        armorInfo = xmlnode.xpath('./*[not(self::type|self::location)]')
+        self.armour = {}
+        for location in armorInfo:
+            self.armour[location.tag.lower()] = int(location.text)
+
+        # Add an entry for the blank as well, just to make sure this gets populated correctly
+        self.armour['--'] = 0
+        
+        armour_type = xmlnode.xpath('./type/text()')[0]
+        self['ssw_name'] = 'Armour - %s' % armour_type
+        self['name'] = armour_type
+        
+        super(SSWArmour,self).__init__()        
+        self.mount(xmlnode, False, False)
+        
+class SSWEngine(SSWMountedItem):
+    default_type = 'E'
+    
+    def __init__(self, xmlnode, gyro_criticals=4):
+        self['ssw_name'] = 'Engine - %s' % xmlnode.text
+        self['name'] = xmlnode.text
+        
+        super(SSWEngine, self).__init__()
+        
+        self.mountings['ct'] = SSWItemMounting('ct', [1,2,3])
+        
+        if self.criticals > 3:
+            offset = 4 + (gyro_criticals or 4)            
+            self.mountings['ct'].add_slot(range(offset, offset+3))
+        
+        side_count = 0
+        if xmlnode.text == 'XL Engine':
+            side_count = 3
+        elif xmlnode.text == 'Light Fusion Engine':
+            side_count = 2
+        elif xmlnode.text == 'XXL Engine':
+            side_count = 6
+            
+        if side_count > 0:
+            right_start = int(xmlnode.get('rsstart')) + 1
+            self.mountings['rt'] = SSWItemMounting('rt', range(right_start, right_start + side_count))
+            
+            left_start = int(xmlnode.get('lsstart')) + 1
+            self.mountings['lt'] = SSWItemMounting('lt', range(left_start, left_start + side_count))
+            
+        self.rating = xmlnode.get('rating')    
+        
+class SSWGyro(SSWMountedItem):
+    default_type = 'G'
+    
+    def __init__(self, xmlnode):
+        self['ssw_name'] = 'Gyro - %s' % xmlnode.text
+        self['name'] = xmlnode.text
+        
+        super(SSWGyro, self).__init__()
+        
+        self.mountings = {}
+        if self.criticals:
+            self.mountings['ct'] = SSWItemMounting('ct', range(4, 4+self.criticals))
+            self.extrapolated = True
+        else:
+            self.mountings['ct'] = SSWItemMounting('ct', [4])
+            self.extrapolated = False
+    
+    def extrapolate(self, criticals):
+        self.criticals = criticals
+        super(SSWGyro, self).extrapolate(criticals)
+            
+class SSWListItem(SSWMountedItem):
+    
+    def __init__(self, item_name, item_type, item_class, xml_location):
+        self['name'] = item_name
+        self['ssw_name'] = '%s - %s' % (item_type, item_name)
+        
+        super(SSWListItem, self).__init__(eq_class=item_class)
+        
+        code = xml_location.text.lower()
+        index = int(xml_location.get('index')) + 1
+        size = (self.criticals or 1)        
+        
+        self.mountings[code] = SSWItemMounting(code, range(index, index+size))
+        
+        
--- solaris/utilities/parser.py
+++ solaris/utilities/parser.py
@@ -2,10 +2,9 @@ from math import floor
 
 from django.utils.html import strip_tags
 
-from solaris.warbook.equipment.models import Equipment
 
 from solaris.utilities import translate
-
+from solaris.utilities.equipment import SSWEquipment, SSWEngine, SSWGyro, SSWArmour, SSWStructure, SSWListItem
 
 class SSWParseError(Exception):
     def __init__(self, mech, formerrors):
@@ -14,185 +13,38 @@ class SSWParseError(Exception):
             self.value += '\t%s: %s' % (key, strip_tags('%s' % value))
     def __str__(self):
         return repr(self.value)
-   
-
-class SSWMountedItem(dict):
-    
-    default_type = '?'   
-    
-    def __init__(self, eq_class=None):
-        self['mech'] = None
-            
-        if eq_class == None:
-            eq_class = self.__class__.default_type
-                
-        if self['ssw_name']:
-            (self.equipment , created) = Equipment.objects.get_or_create(ssw_name=self['ssw_name'])
-            
-            self['equipment'] = self.equipment.id
-            if created:
-                self.equipment.name = self['name']
-                self.equipment.equipment_class = eq_class
-                self.equipment.save()
-            
-            if self.equipment.critical_func:
-                self.criticals = self.equipment.criticals()
-            else:
-                self.criticals = None
-
-    def mount(self, xmlnode, rear_firing=False, turret=False):
-        #Most entries only record a single slot, and require that we 
-        #extrapolate from there.
-        self.extrapolated = False
-        
-        locations = xmlnode.xpath('./location|./splitlocation')
-        
-        self.mountings = {}
-        for loc in locations:
-            if loc.tag == 'location':
-                index = [int(loc.get('index'))+1]
-            if loc.tag == 'splitlocation':
-                count = int(loc.get('number'))
-                start = int(loc.get('index'))
-                #TODO - Check if SSW counts forward or backward from the assigned index
-                index = range(start+1, count+start+1)
-            
-            loc_code = loc.text.lower()
-            
-            if loc.text in self.mountings:
-                self.mountings[loc_code].add_slot(index)
-            else:
-                self.mountings[loc_code] = SSWItemMounting(loc_code, index, rear=rear_firing, turret=turret)
-            
-        if len(self.mountings) == 0:
-            self.mountings['--'] = SSWItemMounting('--', [])
-    
-    def extrapolate(self, criticals):
-        if self.extrapolated:
-            return
-        
-        if len(self.mountings) == 1: 
-            loc = self.mountings.keys()[0]
-            self.mountings[loc].extrapolate(criticals)
-        self.extrapolated = True
 
 class SSWLocation(dict):
     def __init__(self, mech, armour, code):
         self['mech'] = mech
         self['armour'] = armour
 
-class SSWEquipment(SSWMountedItem):
+class SSWItemList(list):
+    item_class = '?'
+    item_group = '?'
+    
     def __init__(self, xmlnode):
-        (self['name'], rear, turret) = self.parse_name(xmlnode.xpath['./name[1]'].text)
-        self.node_type = xmlnode.tag
-        self['ssw_name'] = '%s - %s' % (self.node_type, self.name)
+        item_name = xmlnode.xpath('./type/text()')[0]
         
-        self.mount(xmlnode, rear, turret)
-        super(SSWEquipment,self).__init__()
-        
-    def parse_name(self, equipment_name):
-        rear = False
-        turret = False
-        
-        if equipment_name[0:3] == '(T)':
-            turret = True
-            equipment_name = equipment_name[4:]
-            
-        if equipment_name[0:3] == '(R)':
-            rear = True
-            equipment_name = equipment_name[4:]
-            
-        return (equipment_name, rear, turret)
-        
-class SSWStructure(SSWMountedItem):
-    default_type = 'S'
+        for location in xmlnode.xpath('./location'):
+            self.append(SSWListItem(item_name, self.__class__.item_class, self.__class__.item_group, location))
 
-    def __init__(self, xmlnode):
-        self.mount(xmlnode, False, False)
-        self.extrapolated = True
-        
-        structure_type = xmlnode.xpath('./type/text()')[0]
-        self['ssw_name'] = 'Structure - %s' % structure_type
-        self['name'] = structure_type
- 
-class SSWArmour(SSWMountedItem):
-    
-    default_type = 'S'
-    
-    def __init__(self, xmlnode):
-         
-        #Armour is stored as multiple single-slot assignments, so we can consider
-        #it extrapolated already
-        self.mount(xmlnode, False, False)
-        self.extrapolated = True
-        
-        armorInfo = xmlnode.xpath('./*[not(self::type|self::location)]')
-        self.armour = {}
-        for location in armorInfo:
-            self.armour[location.tag.lower()] = int(location.text)
+class SSWHeatsinkList(SSWItemList):
+    item_class = 'Heatsink'
+    item_group = 'H'
 
-        # Add an entry for the blank as well, just to make sure this gets populated correctly
-        self.armour['--'] = 0
+class SSWJumpjetList(SSWItemList):
+    item_class = 'Jumpjet'
+    item_group = 'J'   
+
+class SSWLoadout(list):
+    def __init__(self, xmlnode):       
+        self += SSWHeatsinkList( xmlnode.xpath('./heatsinks')[0])
         
-        armour_type = xmlnode.xpath('./type/text()')[0]
-        self['ssw_name'] = 'Armour - %s' % armour_type
-        self['name'] = armour_type
+        xml_jets = xmlnode.xpath('./jumpjets')
         
-        super(SSWArmour,self).__init__()
-        
-class SSWEngine(SSWMountedItem):
-    default_type = 'E'
-    
-    def __init__(self, xmlnode, gyro_criticals=4):
-        self['ssw_name'] = 'Engine - %s' % xmlnode.text
-        self['name'] = xmlnode.text
-        
-        self.mountings = {}
-        
-        if xmlnode.text == 'Compact Fusion Engine':
-            self.mountings['ct'] = SSWItemMounting('ct', [1,2,3])
-        else:
-            self.mountings['ct'] = SSWItemMounting('ct', [1,2,3] + range(4 + gyro_criticals, 7 + gyro_criticals))
-        
-        side_count = 0
-        if xmlnode.text == 'XL Engine':
-            side_count = 3
-        elif xmlnode.text == 'Light Fusion Engine':
-            side_count = 2
-        elif xmlnode.text == 'XXL Engine':
-            side_count = 6
-            
-        if side_count > 0:
-            right_start = int(xmlnode.get('rsstart')) + 1
-            self.mountings['rt'] = SSWItemMounting('rt', range(right_start, right_start + side_count))
-            
-            left_start = int(xmlnode.get('lsstart')) + 1
-            self.mountings['lt'] = SSWItemMounting('lt', range(left_start, left_start + side_count))
-            
-        self.rating = xmlnode.get('rating')        
-        
-        super(SSWEngine, self).__init__()
-        
-class SSWGyro(SSWMountedItem):
-    default_type = 'G'
-    
-    def __init__(self, xmlnode):
-        self['ssw_name'] = 'Gyro - %s' % xmlnode.text
-        self['name'] = xmlnode.text
-        
-        super(SSWGyro, self).__init__()
-        
-        self.mountings = {}
-        if self.criticals:
-            self.mountings['ct'] = SSWItemMounting('ct', range(4, 4+self.criticals))
-            self.extrapolated = True
-        else:
-            self.mountings['ct'] = SSWItemMounting('ct', [4])
-            self.extrapolated = False
-    
-    def extrapolate(self, criticals):
-        self.criticals = criticals
-        super(SSWGyro, self).extrapolate(criticals)        
+        if xml_jets:
+            self += SSWJumpjetList(xml_jets[0])
     
 class SSWMech(dict):
     def get_number(self, node, xpath):
@@ -212,18 +64,17 @@ class SSWMech(dict):
             self['bv_value'] = 0
         else:
             self['bv_value'] = self.get_number(xmlnode, './battle_value/text()')
-        
-        self.equipment = []
-        
+            
         self.gyro = SSWGyro( xmlnode.xpath('./gyro')[0] )
         self.engine = SSWEngine( xmlnode.xpath('./engine')[0], gyro_criticals=self.gyro.criticals )
         self.armour = SSWArmour( xmlnode.xpath('./armor')[0] )
-        self.structure = SSWStructure( xmlnode.xpath('./structure')[0] )
-        
+        self.structure = SSWStructure( xmlnode.xpath('./structure')[0] )        
+                
+        self.equipment = SSWLoadout( xmlnode.xpath('./baseloadout')[0] )
         self.equipment.append(self.gyro)
         self.equipment.append(self.engine)
         self.equipment.append(self.armour)        
-        self.equipment.append(self.structure)        
+        self.equipment.append(self.structure) 
         
         self['engine_rating'] = self.engine.rating
         self['stock_design'] = stock
