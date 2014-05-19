1c19fccaafd5a917c27ebd81109c84b69a7fafee
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-19 21:25:41 +1000

    Fix some flaws in cms admin.py

--- solaris/cms/admin.py
+++ solaris/cms/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from solaris.cms import models
 
-admin.site.register(models.NewsPost, NewsPostAdmin)
-
-class NewsPostAdmin(admin.ModelAdmin)
+class NewsPostAdmin(admin.ModelAdmin):
     models = models.NewsPost
     
+admin.site.register(models.NewsPost, NewsPostAdmin)
