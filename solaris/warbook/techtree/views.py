7034c9676eb3ae3805929569409b7193746a9348
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-24 14:00:21 +1000

    Convert Technology list to Django Templates

--- solaris/warbook/techtree/views.py
+++ solaris/warbook/techtree/views.py
@@ -3,6 +3,9 @@ from django_genshi import loader
 from genshi import Markup
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+from solaris.views import SolarisViewMixin
 
 from solaris.warbook.techtree import models
 from solaris.warbook.views import ReferenceView
@@ -10,22 +13,25 @@ from solaris.warbook.views import ReferenceView
 class TechnologyView(ReferenceView):
     submenu_selected = 'TechTree'
 
-class TechnologyListView(TechnologyView):
-    def get(self, request, **kwargs):
-        
-        # Construct Technologies list
-        tech_list = []
-        
-        for (code, name) in models.Technology.categories:
-            tech_list.append(
-                ( name, [(tier, models.Technology.objects.filter(category=code, tier=tier, show=True)) for tier in range(1,4)] )
-            )
+class TechnologyListView(SolarisViewMixin, TemplateView):
+    menu_selected = 'Reference'
+    submenu_selected = 'TechTree'
+    template_name = 'warbook/techlist.tmpl'
     
-        # Render Technologies List
-        tmpl_tech = loader.get_template('warbook/techtree/tech_list.genshi')    
-        techtree = Markup(tmpl_tech.generate(techtree=tech_list, baseURL=request.get_full_path()))
-
-        return HttpResponse(self.in_layout(techtree, request))
+    def get_context_data(self, **kwargs):
+        page_context = super(TechnologyListView, self).get_context_data(**kwargs)
+        tech_list = []
+         
+        for (code, name) in models.Technology.categories:
+            print 'Adding %s (%s)' % (name, code)
+            tech_list.append(
+                { 'name' : name
+                , 'tiers' : [{ 'number' : tier, 'techs' : models.Technology.objects.filter(category=code, tier=tier, show=True)} for tier in range(1,4)]}
+            )
+         
+        page_context['techtree'] = tech_list
+        
+        return page_context
 
 class TechnologyDetailView(TechnologyView):
     def get(self, request, technology='', **kwargs):
