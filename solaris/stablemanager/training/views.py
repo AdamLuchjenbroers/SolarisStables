611e43f5fed41472de864eab0b41e054f3afcad6
AdamLuchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-30 09:29:23 +1000

    Move Training over to Django templates

--- solaris/stablemanager/training/views.py
+++ solaris/stablemanager/training/views.py
@@ -1,13 +1,8 @@
-from solaris.stablemanager.views import StableView
-from genshi import Markup
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-#from django_genshi import loader
+from solaris.stablemanager.views import StableWeekMixin
 
-class StableTrainingView(StableView):
+class StableTrainingView(StableWeekMixin, TemplateView):
     submenu_selected = 'Training'
-    
-    def get(self, request, stable=None, week=None):
-        body = Markup('<P>The Training Ledger for the %s will go here</P><P>The Selected Broadcast Week is: %s</P>' % (stable.stable_name, week))
-        return HttpResponse(self.in_layout(body, request))
+    template_name = 'stablemanager/stable_training.tmpl'
         
