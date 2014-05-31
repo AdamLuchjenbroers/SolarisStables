db94e44ac560cf113b328a00b5f079b68b7d0ec6
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-31 20:19:48 +1000

    URL Tweaks / Fixes

--- solaris/urls.py
+++ solaris/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
     
     (r'^admin/', include(admin.site.urls)),
     (r'^reference/', include('solaris.warbook.urls')),
-    (r'^stable/?', include('solaris.stablemanager.urls'))
+    (r'^stable/', include('solaris.stablemanager.urls'))
 )
 
 if settings.USE_DJANGO_STATIC:
