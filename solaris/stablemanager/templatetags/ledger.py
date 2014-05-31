0c3c83e970001be38ebebf4144730edea96ab398
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-31 16:21:40 +1000

    Add new template tag for rendering Ledger forms

---
+++ solaris/stablemanager/templatetags/ledger.py
@@ -0,0 +1,43 @@
+from django.template import Context, loader, Library, Node, TemplateSyntaxError, Variable
+from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
+
+register = Library()
+
+class LedgerFormNode(Node):
+    def __init__(self, form_variable, action='Add'):
+        self.week = Variable('week.week_number')
+        self.form = Variable(form_variable)
+        
+        self.template = loader.get_template('stablemanager/stable_ledger_itemform.tmpl')
+        self.node_context = Context( {'action' : action} )
+        
+        
+    def render(self, context):
+        week = Variable('week.week_number').resolve(context)
+        
+        if week:
+            self.node_context['post_url'] = reverse('stable_ledger', kwargs={'week': week})
+        else:
+            self.node_context['post_url'] = reverse('stable_ledger_now')
+            
+        self.node_context['form'] = self.form.resolve(context)
+        
+        return mark_safe(self.template.render(self.node_context))       
+    
+
+def do_ledger_form(parser, token, action='Add'):
+    arguments = token.split_contents()
+    
+    if len(arguments) != 2:
+        raise TemplateSyntaxError("%r tag expects a single  argument" % token.contents.split()[0])
+    
+    return LedgerFormNode(arguments[1], action=action)
+
+@register.tag(name="ledger_add_form")
+def do_ledger_add_form(parser, token):
+    return do_ledger_form(parser, token, action='Add')
+
+@register.tag(name="ledger_edit_form")
+def do_ledger_edit_form(parser, token):
+    return do_ledger_form(parser, token, action='Edit')
