ab1f261195fbd5085c9e9d6dee04cbaa5d613445
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-05-31 16:25:49 +1000

    Use new template tag format for stable ledger

--- solaris/stablemanager/ledger/forms.py
+++ solaris/stablemanager/ledger/forms.py
@@ -1,12 +1,8 @@
 from django.forms import ModelForm, Form, HiddenInput, IntegerField
 
-from solaris.forms import SolarisFixedFormMixin
 from solaris.stablemanager.ledger.models import LedgerItem
 
-class LedgerItemForm(SolarisFixedFormMixin, ModelForm):
-    
-    inner_form_template = 'stablemanager/ledger_item_add.genshi'
-    
+class LedgerItemForm(ModelForm):
     id = IntegerField(widget=HiddenInput, required=False)
     
     def __init__(self, *args, **kwargs):
@@ -30,28 +26,13 @@ class LedgerItemForm(SolarisFixedFormMixin, ModelForm):
         
     def set_postURL(self, postURL):
         self.postURL = postURL
-        
-    def as_p(self):
-        return self.template.generate(
-             fields = self.getAllFields()
-        ,    submit = self.submit_action
-        ,    post_url = self.postURL
-        ,    submit_tab = self.submit_tab
-        )
                 
     class Meta:
         model = LedgerItem
         
-class LedgerDeleteForm(SolarisFixedFormMixin, Form):                      
-    inner_form_template = 'stablemanager/ledger_item_delete.genshi'    
+class LedgerDeleteForm(Form): 
     id = IntegerField(widget=HiddenInput, required=True)
     week = IntegerField(widget=HiddenInput, required=True)
-    
-    def as_p(self):
-        return self.template.generate(
-             fields = self.getAllFields()
-        ,    submit = 'X'
-        ,    post_url = '/stable/ledger/delete'
-        )
+
 
     
