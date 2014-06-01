bbb4ab830d0bdedacb0985554291b53635f475a7
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2014-06-01 21:20:15 +1000

    Add basic columnfield layout and use it to try and make pilot form prettier.

--- solaris/templatetags/solaris_forms.py
+++ solaris/templatetags/solaris_forms.py
@@ -3,16 +3,17 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-class BasicFieldNode(template.Node):
-    def __init__(self, field):
+class TemplateFieldNode(template.Node):
+    def __init__(self, field, template_name='tags/form_basicfield.tmpl'):
         self.field = template.Variable(field)
-        self.template = template.loader.get_template('tags/form_basicfield.tmpl')
+        self.template = template.loader.get_template(template_name)
     
     def render(self, context):
         field_object = self.field.resolve(context)
         
         node_context = template.Context( {'field' : field_object }) 
         return mark_safe(self.template.render(node_context))
+
     
 
 @register.tag(name="basic_field")
@@ -23,6 +24,14 @@ def do_basic_field(parser, token):
     except ValueError:
         raise template.TemplateSyntaxError("%r tag expects a single argument" % token.contents.split()[0])
     
-    return BasicFieldNode(field)
+    return TemplateFieldNode(field)
 
-
+@register.tag(name="column_field")
+def do_column_field(parser, token):
+    try:
+        (tag_name, field) = token.split_contents()
+        
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag expects a single argument" % token.contents.split()[0])
+    
+    return TemplateFieldNode(field, template_name='tags/form_columnfield.tmpl')
