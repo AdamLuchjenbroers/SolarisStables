9f07711b69796aae591c47d0f25c7270f942b26b
AdamLuchjenbroers <adam.luchjenbroers@gmail.com>
2014-06-02 18:18:36 +1000

    Use TemplateView since using CreateView with multiple forms gets a bit clumsy

--- solaris/stablemanager/assets/views.py
+++ solaris/stablemanager/assets/views.py
@@ -13,9 +13,12 @@ class StablePilotsView(StableWeekMixin, TemplateView):
 class StableNewPilotsView(StableViewMixin, TemplateView):
     submenu_selected = 'Pilots'
     template_name = 'stablemanager/forms/form_pilots.tmpl'
-    success_url = reverse ('pilots_add')
 
-    def create_additional_forms(self, pilot=None):
+    def create_forms(self, pilot=None):
+        pilot_initial = {
+            'stable' : self.stable.id
+        ,   'affiliation' : self.stable.house.id
+        }
         pilotweek_initial = {
             'pilot_rank' : PilotRank.objects.get(rank='Rookie')
         ,   'skill_gunnery' : 5
@@ -23,46 +26,47 @@ class StableNewPilotsView(StableViewMixin, TemplateView):
         ,   'start_character_points' : 0
         ,   'week' : self.stable.current_week
         }        
+                
         if pilot:
             pilotweek_initial['pilot'] = pilot
         
-        
         if self.request.POST:
-            pilotweek = forms.PilotWeekForm(self.request.POST, initial=pilotweek_initial)
-            skillset = forms.PilotInlineSkillsForm(self.request.POST)
+            self.form_pilot = forms.PilotForm(self.request.POST, initial=pilot_initial, instance=pilot)
+            self.form_pilotweek = forms.PilotWeekForm(self.request.POST, initial=pilotweek_initial)
+            self.form_skillset = forms.PilotInlineSkillsForm(self.request.POST)
         else:
-            pilotweek = forms.PilotWeekForm(initial=pilotweek_initial)
-            skillset = forms.PilotInlineSkillsForm()
-        return pilotweek, skillset 
-            
-    def get_initial(self):
-        return {
-            'stable' : self.stable
-        }
+            self.form_pilot = forms.PilotForm(initial=pilot_initial, instance=pilot)
+            self.form_pilotweek = forms.PilotWeekForm(initial=pilotweek_initial)
+            self.form_skillset = forms.PilotInlineSkillsForm()
+        return self.form_pilot, self.form_pilotweek, self.form_skillset 
    
     def get_context_data(self, **kwargs):
         page_context = super(StableNewPilotsView, self).get_context_data(**kwargs)
         
-        page_context['pilot'] = forms.PilotForm(initial={
-            'stable' : self.stable.id
-        ,   'affiliation' : self.stable.house.id
-        })
+        page_context['pilot'] = self.form_pilot
+        page_context['pilotweek'] = self.form_pilotweek
+        page_context['skillset'] = self.form_skillset
         
-        ( page_context['pilotweek'], page_context['skillset']) = self.create_additional_forms()
-        
-        page_context['post_url'] = reverse ('pilots_add')
+        page_context['post_url'] = '/' # reverse('pilots_add')
         page_context['submit'] = 'Add'
         page_context['form_class'] = 'pilot'
         
         return page_context
     
+    def get(self, request):
+        self.create_forms()
+        return super(StableNewPilotsView, self).get(request)
+    
+    def post(self, request):
+        self.create_forms()
+        return self.get(request)
+
     def form_valid(self, form):        
         self.object = form.save(commit=False)
         
-        (pilotweek, skillset) = self.create_additional_forms(pilot=self.object)
-        if pilotweek.is_valid():
-            skillset.instance = pilotweek.instance
-            if skillset.isv_valid():
-                form.save()
-                pilotweek.save()
-                skillset.save()
+        if self.form_pilotweek.is_valid():
+            self.form_skillset.instance = self.form_pilotweek.instance
+            if self.form_skillset.is_valid():
+                self.form_pilot.save()
+                self.form_pilotweek.save()
+                self.form_skillset.save()
