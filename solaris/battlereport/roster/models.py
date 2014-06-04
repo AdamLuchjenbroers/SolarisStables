40e997cb043414fc13bb96412194c9d511ecb479
AdamLuchjenbroers <adam.luchjenbroers@gmail.com>
2014-06-04 18:56:42 +1000

    Add Meta for roster models

--- solaris/battlereport/roster/models.py
+++ solaris/battlereport/roster/models.py
@@ -6,16 +6,41 @@ from solaris.battlereport.models import BroadcastWeek
 
 class FightGroup(models.Model):
     name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'Fight Groups'
+        verbose_name = 'Fight Group'
+        db_table = 'battlereport_fightgroup'
+        app_label = 'battlereport'
     
 class FightType(models.Model):
     group = models.ForeignKey(FightGroup)
     rules = MarkupField()
+    is_simulation = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Fight Types'
+        verbose_name = 'Fight Type'
+        db_table = 'battlereport_fighttype'
+        app_label = 'battlereport'
 
 class Map(models.Model):
     name = models.CharField(max_length=20)
     special_rules = MarkupField()
 
+    class Meta:
+        verbose_name_plural = 'Maps'
+        verbose_name = 'Map'
+        db_table = 'battlereport_map'
+        app_label = 'battlereport'
+
 class RosteredFight(models.Model):
     week = models.ForeignKey(BroadcastWeek)
     fight_type = models.ForeignKey(FightType)
     fought = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Rostered Fights'
+        verbose_name = 'Rostered Fight'
+        db_table = 'battlereport_rosteredfight'
+        app_label = 'battlereport'
