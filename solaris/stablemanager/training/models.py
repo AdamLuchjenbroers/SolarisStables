3923707d9f8043c2198f5eebd95057b3c443acad
AdamLuchjenbroers <adam.luchjenbroers@gmail.com>
2014-06-09 13:04:00 +1000

    Add skeleton training model

---
+++ solaris/stablemanager/training/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from solaris.warbook.pilotskill.models import TrainingCost, PilotTrait
+from solaris.stablemanager.assets.models import PilotWeek
+
+class TrainingEvent(models.Model):
+    pilot = models.ForeignKey(PilotWeek)
+    training = models.ForeignKey(TrainingCost)
+    skill = models.ForeignKey(PilotTrait, null=True, blank=True)
+    notes = models.CharField(max_length=50, null=True, blank=True)
