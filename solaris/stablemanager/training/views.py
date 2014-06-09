a256faf65e62654781a0fbc925ec73b6cdcade32
AdamLuchjenbroers <adam.luchjenbroers@gmail.com>
2014-06-09 14:08:53 +1000

    Use ListView for Training list

--- solaris/stablemanager/training/views.py
+++ solaris/stablemanager/training/views.py
@@ -1,16 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from django.shortcuts import get_list_or_404
 
 from solaris.stablemanager.views import StableWeekMixin
 from solaris.stablemanager.assets.models import PilotWeek
 
-class StableTrainingView(StableWeekMixin, TemplateView):
+class StableTrainingView(StableWeekMixin, ListView):
     submenu_selected = 'Training'
     template_name = 'stablemanager/stable_training.tmpl'
     
-    def get_context_data(self, **kwargs):
-        page_context = super(StableTrainingView, self).get_context_data(**kwargs)
-        
-        page_context['object_list'] = get_list_or_404(PilotWeek, pilot__stable=self.stable, week=self.week)
-        return page_context
+    def get_queryset(self, **kwargs):
+        return get_list_or_404(PilotWeek, pilot__stable=self.stable, week=self.week)
         
