9e7104dfedfe6d9692f3a65b1f44a5890ef8e0c9
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-10-27 20:02:24 +1100

    Specify tablename for house x discipline table

--- solaris/warbook/models.py
+++ solaris/warbook/models.py
@@ -4,7 +4,7 @@ from solaris.warbook.pilotskill.models import PilotTraitGroup
 class House(models.Model):
     house = models.CharField(max_length=20, unique=True)
     blurb = models.TextField()
-    house_disciplines = models.ManyToManyField(PilotTraitGroup)
+    house_disciplines = models.ManyToManyField(PilotTraitGroup, db_table='warbook_house_x_discipline')
 
     class Meta:
         verbose_name_plural = 'Houses'
