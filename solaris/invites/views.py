5e9c2af8b1f48ccb7e39e925531f5b92fb062ba2
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-07 22:58:25 +1100

    Fix minor indentation issue

--- solaris/invites/views.py
+++ solaris/invites/views.py
@@ -4,14 +4,14 @@ from invitations.views import SendInvite, AcceptInvite
 from solaris.views import SolarisViewMixin
 
 class SolarisSendInvite(SolarisViewMixin, SendInvite):
-   template_name='invites/send_invite.html'
-   submit='Invite'
+    template_name='invites/send_invite.html'
+    submit='Invite'
 
-   def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs):
         page_context = super(SolarisSendInvite, self).get_context_data(**kwargs)
         
         page_context['submit'] = 'Invite'
         return page_context
 
 class SolarisAcceptInvite(SolarisViewMixin, SendInvite):
-   template_name='invites/accept_invite.html'
+    template_name='invites/accept_invite.html'
