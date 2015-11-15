50a5017554dfc6add4961328da13fe668d0dc95a
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-15 17:40:33 +1100

    Rename mechs bulk importer.

--- solaris/utilities/csv/mechs.py
+++ solaris/utilities/csv/mechs.py
@@ -8,18 +8,21 @@ from solaris.utilities.loader import SSWLoader
 
 sswPattern = re.compile('.*\.ssw$')
 
-failures = {}
-
-def recursiveScanAll(path, relative_path='.'):
-   
+def loadMechFolder(path=settings.SSW_STOCK_MECHS_ROOT, relative_path='.', basepath=None):
+    failures = {}
+    
+    if basepath == None:
+        #We're the first call
+        basepath = path
+       
     for fileName in os.listdir(path):
         fullpath = path + '/' + fileName    
         if os.path.isdir(fullpath):
-            recursiveScanAll(fullpath, relative_path=relative_path + '/' + fileName)
+            loadMechFolder(path=fullpath, relative_path=relative_path + '/' + fileName, basepath=basepath)
             
         if os.path.isfile(fullpath) and sswPattern.match(fileName):
             try: 
-                loader = SSWLoader(relative_path  + '/' + fileName)
+                loader = SSWLoader(relative_path + '/' + fileName, basepath=basepath)
                 loader.load_mechs()
             except BaseException as e:
                 print '%s > %s ' % (relative_path, e)
@@ -28,17 +31,12 @@ def recursiveScanAll(path, relative_path='.'):
             finally:
                 #Explicitly free memory, because letting them pile up is a serious memory hog
                 del loader   
-    
-if __name__ == '__main__':
-    
-    basepath = settings.SSW_STOCK_MECHS_ROOT
         
-    recursiveScanAll(basepath)
-    
-    for file, errors in failures.items():
-        print 'Errors encountered processing %s' % file
+    for fileName, errors in failures.items():
+        print 'Errors encountered processing %s' % fileName
         
         for err in errors:
             print '\t%s' % err
     
+
     
