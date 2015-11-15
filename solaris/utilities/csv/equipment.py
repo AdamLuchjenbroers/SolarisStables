31e498b5cc86288e50496d1930ef8a730bfa46b4
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-15 14:02:43 +1100

    Improve Equipment Loader considerably
    - Remove random print header list
    - Add a count of items exported
    - Allow filter conditions to be passed as parameters to dumpEquipmentCSV
    - Explicitly specify model fields as requested for Django 1.7+
    - Improve loader message to clarify if new or updated

--- solaris/utilities/csv/equipment.py
+++ solaris/utilities/csv/equipment.py
@@ -4,25 +4,33 @@ from django.forms.models import model_to_dict
 
 from solaris.warbook.equipment.models import Equipment
 
+equipment_fields = ['id', 'name', 'ssw_name', 'equipment_class'
+                   , 'tonnage_func', 'tonnage_factor', 'critical_func', 'critical_factor'
+                   , 'cost_func', 'cost_factor', 'weapon_properties', 'basic_ammo'
+                   , 'ammo_for', 'has_ammo', 'ammo_size', 'splittable', 'crittable'
+                   , 'evaluate_last', 'record_status', 'fcs_artemis_iv', 'fcs_artemis_v'
+                   , 'fcs_apollo' ]
+
 class EquipmentForm(ModelForm):
     class Meta:
         model = Equipment
+        fields = equipment_fields
 
 def getLauncher(launcher):
     try:
-        ammo_for = Equipment.objects.get(id=int(row['ammo_for']))
+        ammo_for = Equipment.objects.get(id=int(launcher))
         return ammo_for
     except (Equipment.DoesNotExist, ValueError):
         pass
     
     try:
-        ammo_for = Equipment.objects.get(name=row['ammo_for'])
+        ammo_for = Equipment.objects.get(name=launcher)
         return ammo_for
     except Equipment.DoesNotExist:
         pass   
     
     try:
-        ammo_for = Equipment.objects.get(ssw_name=row['ammo_for'])
+        ammo_for = Equipment.objects.get(ssw_name=launcher)
         return ammo_for
     except Equipment.DoesNotExist:
         pass
@@ -51,25 +59,22 @@ def loadEquipmentCSV(csvfile):
         
         eq = EquipmentForm(row, instance=eq_instance)
         if eq.is_valid():
-            print 'Loaded %s' % row['ssw_name']
+            print '%s %s' % ('Loaded' if eq_instance == None else 'Updated', row['ssw_name'])
             eq.save()
             
-        fh.close()
+    fh.close()
 
-def dumpEquipmentCSV(csvfile):
+def dumpEquipmentCSV(csvfile, **kwargs):
     fh = open(csvfile,'w')
 
-    to_fill = Equipment.objects.filter(record_status=0)
-    header = ['id', 'name', 'ssw_name', 'equipment_class'
-            , 'tonnage_func', 'tonnage_factor', 'critical_func', 'critical_factor', 'cost_func', 'cost_factor', 'weapon_properties'
-            , 'basic_ammo', 'ammo_for', 'has_ammo', 'ammo_size', 'splittable', 'crittable', 'evaluate_last', 'record_status', 'fcs_artemis_iv', 'fcs_artemis_v', 'fcs_apollo' ]
-    
-    writer = DictWriter(fh, fieldnames=header)
-    print header
-    
+    to_fill = Equipment.objects.filter(**kwargs)
+
+    writer = DictWriter(fh, fieldnames=equipment_fields)   
     writer.writeheader()
     
     for item in to_fill:
         writer.writerow( model_to_dict(item) )
+        
+    print "%i Equipment Items exported" % len(to_fill)
         
     fh.close()
