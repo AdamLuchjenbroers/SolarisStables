c79520f7fed6e4af1b9af73598d52e3feef4318a
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-15 17:40:04 +1100

    Syntax and indentation fixes.

--- solaris/utilities/csv/MechImporter.py
+++ solaris/utilities/csv/MechImporter.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     ssw_file = sys.argv[1]
 
     try: 
-	loader = SSWLoader(ssw_file)
-	loader.load_mechs()
-	del loader   
+        loader = SSWLoader(ssw_file)
+        loader.load_mechs()
+        del loader   
     except BaseException as e:
-	print '%s > %s ' % (ssw_file, e)
-	traceback.print_exc()
+        print '%s > %s ' % (ssw_file, e)
+        traceback.print_exc()
