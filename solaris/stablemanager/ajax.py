f6cb330815b57c2952b8421fd20ce86753c21642
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-17 21:18:25 +1100

    Restrict dropdown to currently producible mech designs.

--- solaris/stablemanager/ajax.py
+++ solaris/stablemanager/ajax.py
@@ -5,16 +5,18 @@ from django.http import HttpResponse
 from django.views.generic import View
 
 from .views import StableWeekMixin
-from .models import StableWeek
 
 class ProductionChassisAutocomplete(StableWeekMixin, View):
     def get(self, request):
+        print "[%s]" % request.GET['m']
         production = self.stable.house.produced_designs.filter(mech_name__icontains=request.GET['m'])
+        
+        equipment_list = self.stableweek.available_equipment()
   
         chassis_list = {}
         
         for mech in production.all():
-            # TODO: Check available equipment
-            chassis_list[mech.mech_name] = True
+            if mech.can_be_produced_with(equipment_list):
+                chassis_list[mech.mech_name] = True
 
         return HttpResponse(json.dumps(chassis_list.keys())) 
