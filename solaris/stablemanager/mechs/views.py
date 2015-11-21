ae95d6aea255aa43b860b1b8e9bad609af5499e4
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-22 10:32:29 +1100

    Use a FormView instead of a TemplateView as base

--- solaris/stablemanager/mechs/views.py
+++ solaris/stablemanager/mechs/views.py
@@ -1,17 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
 
 from solaris.stablemanager.views import StableViewMixin
 
 from . import forms, models
 
-class InitialMechPurchaseView(StableViewMixin, TemplateView):
+class InitialMechPurchaseView(StableViewMixin, FormView):
     # Looks like Formset stuff isn't in Django main yet, so we'll have to improvise
     template_name = 'stablemanager/initial_mechs.tmpl'
+    form_class = forms.InitialMechsForm
+    success_url = '/stable/initial-pilots'
     
     def get_context_data(self, **kwargs):
         context = super(InitialMechPurchaseView, self).get_context_data(**kwargs)
         
-        context['mech_formset'] = forms.InitialMechsForm()
         context['submit'] = 'Purchase All'
         context['form_class'] = 'list initial-mechs'
         context['initial_balance'] = self.stable.get_stableweek().opening_balance
