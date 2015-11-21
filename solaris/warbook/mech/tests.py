0e0aa4292a8c434716456c30d4ba8b9c54413da1
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-22 08:50:27 +1100

    Mech data now loaded as part of migrations, test case setup not needed

--- solaris/warbook/mech/tests.py
+++ solaris/warbook/mech/tests.py
@@ -3,11 +3,6 @@ from .models import MechDesign
 
 class MechMathsTest(TestCase):
     
-    def setUp(self):
-        MechDesign.objects.create(mech_name='Wolverine', mech_code='WVR-7D', tonnage=55, engine_rating=275, credit_value=11214457, bv_value=1314 )
-        MechDesign.objects.create(mech_name='Raven', mech_code='RVN-4L', tonnage=35, engine_rating=210, credit_value=6001425, bv_value=873 )
-        
-    
     def test_walkingMP(self):
         """
         Tests that the engine 'size' formula is implemented correctly
@@ -21,6 +16,7 @@ class MechMathsTest(TestCase):
         Tests that running MP is calculated correctly
         """
         raven = MechDesign.objects.get(mech_name='Raven', mech_code='RVN-4L')
+        self.assertEqual(raven.move_walk(), 6, 'Failed Test: Expected walking MP of 6, got %i' % raven.move_walk())
         self.assertEqual(raven.move_run(), 9, 'Failed Test: Testing Running MP Formula (even), expected 9, got %i' % raven.move_run())
         
     def test_runningMP_Odd(self):
