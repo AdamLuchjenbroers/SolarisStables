b4a2a96eff9bbaf79f9104ce09da8057e8921541
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-21 21:42:46 +1100

    Build updated mech supply list as part of refactor process

--- solaris/stablemanager/migrations/0002_refactor_stable.py
+++ solaris/stablemanager/migrations/0002_refactor_stable.py
@@ -3,6 +3,35 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+    
+def build_production_list(apps, schema_editor):
+    StableWeek = apps.get_model('stablemanager', 'StableWeek')
+    Equipment = apps.get_model('warbook', 'Equipment')
+    
+    for sw in StableWeek.objects.all():
+        #
+        # refresh_supply_mechs() (Not available in the migration as it's a new function.   
+        #
+        eq_set = Equipment.objects.none()
+
+        for contract in sw.supply_contracts.all():
+            eq_set |= contract.access_to.all()
+
+        mechList = None        
+        if sw.supply_contracts.filter(name='Omnimechs').exists():
+            mechList = sw.stable.house.produced_designs.all()
+        else:
+            mechList = sw.stable.house.produced_designs.filter(is_omni=False)
+                        
+        for mech in mechList:
+            mech_eq = Equipment.objects.filter(id__in=mech.loadout.all().values('equipment'))
+            if (mech_eq.exclude(pk__in=eq_set).count() < 1):
+                sw.supply_mechs.add(mech)
+    
+def noop(apps, schema_editor):
+    # Why bother to delete from tables that are being dropped in the
+    # same operation.
+    pass
 
 class Migration(migrations.Migration):
 
@@ -42,8 +71,21 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='stableweek',
-            name='supply_contracts',
-            field=models.ManyToManyField(to='warbook.Technology'),
+            name='supply_mechs',
+            field=models.ManyToManyField(to='warbook.MechDesign'),
             preserve_default=True,
         ),
+        migrations.AlterField(
+            model_name='pilotweek',
+            name='week',
+            field=models.ForeignKey(related_name='pilots', to='stablemanager.StableWeek'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='stablemechweek',
+            name='stableweek',
+            field=models.ForeignKey(related_name='mechs', blank=True, to='stablemanager.StableWeek', null=True),
+            preserve_default=True,
+        ),
+        migrations.RunPython(build_production_list, reverse_code=noop),
     ]
