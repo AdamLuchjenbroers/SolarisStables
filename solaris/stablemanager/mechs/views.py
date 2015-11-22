5b06cf08da71adbc8f82f3828f43ffbffe479e39
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-22 11:02:23 +1100

    Add selected mechs to stable

--- solaris/stablemanager/mechs/views.py
+++ solaris/stablemanager/mechs/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import FormView
 
 from solaris.stablemanager.views import StableViewMixin
+from solaris.stablemanager.ledger.models import LedgerItem
 
 from . import forms, models
 
@@ -9,6 +10,28 @@ class InitialMechPurchaseView(StableViewMixin, FormView):
     template_name = 'stablemanager/initial_mechs.tmpl'
     form_class = forms.InitialMechsForm
     success_url = '/stable/initial-pilots'
+
+    def form_valid(self, form):
+        for mechform in form:
+            self.stablemech = models.StableMech.objects.create(stable=self.stable, purchased_as=mechform.design)
+            self.stablemechweek = models.StableMechWeek.objects.create(
+              stableweek = self.stable.get_stableweek()
+            , stablemech = self.stablemech
+            , current_design = mechform.design
+            )
+
+            self.ledgeritem = LedgerItem.objects.create (
+              ledger = self.stable.get_stableweek()
+            , description = 'Purchase - %s' % mechform.design.__unicode__()
+            , cost = -mechform.design.credit_value
+            , type = 'P'
+            , tied = True
+            , ref_mechdesign = mechform.design
+            , ref_stablemech = self.stablemech
+            , ref_stablemech_week = self.stablemechweek
+            )
+ 
+        return super(InitialMechPurchaseView, self).form_valid(form)
     
     def get_context_data(self, **kwargs):
         context = super(InitialMechPurchaseView, self).get_context_data(**kwargs)
