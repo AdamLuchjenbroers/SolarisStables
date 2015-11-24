5dd48adedd56577a1b73043207373a22d6b9cbbf
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-25 09:43:34 +1100

    Fix missing import

--- solaris/stablemanager/models.py
+++ solaris/stablemanager/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ObjectDoesNotExist
