fdebada76cdfb76422f6aaf78072ef50fb08fd7a
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-25 17:50:21 +1100

    Add re-calculation of ledger totals

--- static/js/stable_ledger.js
+++ static/js/stable_ledger.js
@@ -1,4 +1,36 @@
 
+function recalc_group(group) {
+   var grouptotal = 0;
+   
+  $('div.item-group-' + group + ' div.ledger-view-display span.ledger-value p').each( function() {
+    cost = parseInt( $(this).text());
+
+    if (!isNaN(cost)) {
+      grouptotal += cost;
+    }
+  });
+
+  if (grouptotal >= 0) {
+    $('#ledger-sumvalue-' + group).text(grouptotal).removeClass('ledger-negative');
+  } else if (grouptotal < 0) {
+    $('#ledger-sumvalue-' + group).text(grouptotal).addClass('ledger-negative');
+  }
+
+  return grouptotal;
+}
+
+function recalc_all() {
+  var total = parseInt( $('#ledger-opening-balance').text());
+
+  $.each(['r', 'p', 'e', 'w', 'i'], function(i, group) {
+      total += recalc_group(group);
+  });
+
+  $('#ledger-closing-balance').text(total); 
+}
+
+// ['r', 'p', 'e', 'w', 'i'] 
+
 $.fn.extend ({
 	showForm : function () {
 		this.find('div.ledger-view-display').hide();
@@ -19,4 +51,6 @@ $( document ).ready(function() {
 		$('div.ledger-item-edit').showItem();		
 		$(this).showForm();
 	});
+
+        recalc_all();
 });
