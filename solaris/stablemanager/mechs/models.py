d3cef6239ffa1d188a458a5562f312020e10aaa9
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-26 17:41:51 +1100

    Add related_name for signature mechs

--- solaris/stablemanager/mechs/models.py
+++ solaris/stablemanager/mechs/models.py
@@ -6,7 +6,6 @@ from solaris.stablemanager.models import Stable, StableWeek
 class StableMech(models.Model):
     stable = models.ForeignKey(Stable, blank=True, null=True)
     purchased_as = models.ForeignKey('warbook.MechDesign')
-    signature_of = models.ForeignKey('Pilot', blank=True, null=True)
     
     class Meta:
         verbose_name_plural = 'Mechs'
@@ -24,7 +23,7 @@ class StableMechWeek(models.Model):
     stableweek = models.ForeignKey(StableWeek, related_name='mechs', blank=True, null=True)
     stablemech = models.ForeignKey(StableMech)
     current_design = models.ForeignKey('warbook.MechDesign')
-    signature_of = models.ForeignKey('Pilot', blank=True, null=True)
+    signature_of = models.ForeignKey('Pilot', related_name='signature_mechs', blank=True, null=True)
 
     objects = StableMechWeekManager()
     
