12407dc7361a8d512dc4986474a0353406fd0591
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-29 10:47:10 +1100

    Basic mech-loader test-cases

--- solaris/utilities/test_mechloader.py
+++ solaris/utilities/test_mechloader.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.exceptions import ObjectDoesNotExist
 
 from solaris.warbook.mech.models import MechDesign
 
@@ -28,6 +29,17 @@ class MechTestMixin(object):
         self.mp_comparison(self.mech.move_jump(), self.__class__.movement_profile[2]
                           , 'Jumping MP Failed, expected %i, got %i')
 
+    def assertEquipment(self, location, slots, ssw_name):
+        try:
+            mount = self.mech.locations.get(location__location=location).criticals.get(slots=slots)
+        except ObjectDoesNotExist:
+            #Deliberately fail here, since apparently we couldn't find the object in these slots
+            self.assertTrue(False, "Unable to find mounting in %s, slots %s" % (location, slots))
+
+        mounted_name = mount.equipment.equipment.ssw_name
+        self.assertEqual(mounted_name, ssw_name, "Object mounted in %s [%s] is %s, not %s" % (location, slots, mounted_name, ssw_name))
+
+
 class WolverineTests(MechTestMixin, TestCase):
     mech_ident = {
         'mech_name'    : 'Wolverine'
@@ -35,6 +47,16 @@ class WolverineTests(MechTestMixin, TestCase):
     ,   'omni_loadout' : 'Base'
     }
     movement_profile = [5,8,5]
+
+    def test_hands(self):
+        self.assertEquipment('LA','4','Actuator - Hand')
+        self.assertEquipment('RA','4','Actuator - Hand')
+
+    def test_engine(self):
+        self.assertEquipment('CT','1,2,3,8,9,10','Engine - XL Engine')
+
+    def test_engine(self):
+        self.assertEquipment('CT','4,5,6,7','Gyro - Standard Gyro')
 
 class RavenTests(MechTestMixin, TestCase):
     mech_ident = {
