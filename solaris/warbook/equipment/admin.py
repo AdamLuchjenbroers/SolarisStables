7b6672ebdd53910ebd1cf1e53be889b51bcdc225
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-30 17:59:22 +1100

    #24 - Add ability to edit Tech <-> Equipment relationships via Django Admin

--- solaris/warbook/equipment/admin.py
+++ solaris/warbook/equipment/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from solaris.warbook.equipment import models
+from solaris.warbook.techtree.models import Technology
+  
+
+class EquipmentTechnologyInline(admin.TabularInline):
+    model = Technology.access_to.through
   
 class EquipmentAdmin(admin.ModelAdmin):
     model = models.Equipment
     list_filter = ('equipment_class', 'record_status')
+    inlines = [EquipmentTechnologyInline,]
     
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "ammo_for":
--- solaris/warbook/techtree/admin.py
+++ solaris/warbook/techtree/admin.py
@@ -4,7 +4,11 @@ from solaris.warbook.techtree import models
 class TechnologyRollModifierInline(admin.StackedInline):
     model = models.TechnologyRollModifier
     extra = 0
+
+class TechnologyEquipmentInline(admin.TabularInline):
+    model = models.Technology.access_to.through
+
   
 class TechnologyAdmin(admin.ModelAdmin):
     fields = ['name', 'urlname', 'description', 'tier', 'category', 'base_difficulty', 'show']
-    inlines = [TechnologyRollModifierInline,]
+    inlines = [TechnologyRollModifierInline,TechnologyEquipmentInline,]
