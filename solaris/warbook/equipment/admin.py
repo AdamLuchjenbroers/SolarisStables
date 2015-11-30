282d8f18569f1ecfecfc8617badbaa0150fac728
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-11-30 20:10:14 +1100

    #24 - Add Has Equipment / Has Technology sidebar filters

--- solaris/warbook/equipment/admin.py
+++ solaris/warbook/equipment/admin.py
@@ -3,7 +3,6 @@ from django import forms
 
 from solaris.warbook.equipment import models
 from solaris.warbook.techtree.models import Technology
-  
 
 class EquipmentForm(forms.ModelForm):
     technology = forms.ModelMultipleChoiceField(
@@ -15,9 +14,24 @@ class EquipmentForm(forms.ModelForm):
                    queryset = models.Equipment.objects.filter(has_ammo=True)
                  , label = 'Ammunition For'
                  )
+
+class HasTechnologyFilter(admin.SimpleListFilter):
+    title = 'Has Technology'
+    parameter_name = 'has_equip'
+
+    def lookups(self, request, model_admin):
+        return ( ('Y','Yes'), ('N','No') )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'Y': 
+            return queryset.filter(supplied_by__isnull=False).distinct()
+        elif self.value() == 'N':
+            return queryset.filter(supplied_by__isnull=True)
+        else:
+            return queryset
   
 class EquipmentAdmin(admin.ModelAdmin):
     model = models.Equipment
     form = EquipmentForm
-    list_filter = ('equipment_class', 'record_status')
+    list_filter = ('equipment_class', 'record_status', HasTechnologyFilter)
             
--- solaris/warbook/techtree/admin.py
+++ solaris/warbook/techtree/admin.py
@@ -4,8 +4,24 @@ from solaris.warbook.techtree import models
 class TechnologyRollModifierInline(admin.StackedInline):
     model = models.TechnologyRollModifier
     extra = 0
+
+class HasEquipmentFilter(admin.SimpleListFilter):
+    title = 'Has Equipment'
+    parameter_name = 'has_equip'
+
+    def lookups(self, request, model_admin):
+        return ( ('Y','Yes'), ('N','No') )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'Y': 
+            return queryset.filter(access_to__isnull=False).distinct()
+        elif self.value() == 'N':
+            return queryset.filter(access_to__isnull=True)
+        else:
+            return queryset
   
 class TechnologyAdmin(admin.ModelAdmin):
     fields = ['name', 'urlname', 'description', 'tier', 'category', 'base_difficulty', 'show', 'access_to']
     filter_horizontal = ('access_to',)
     inlines = [TechnologyRollModifierInline,]
+    list_filter = ('tier', HasEquipmentFilter )
