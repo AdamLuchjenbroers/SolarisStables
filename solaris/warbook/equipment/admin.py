b4e123530c14d701a836723bb229db6a405f41ce
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-12-05 13:24:06 +1100

    Layout improvements for equipment admin form

--- solaris/warbook/equipment/admin.py
+++ solaris/warbook/equipment/admin.py
@@ -5,17 +5,36 @@ from solaris.warbook.equipment import models
 from solaris.warbook.techtree.models import Technology
 
 class EquipmentForm(forms.ModelForm):
-    technology = forms.ModelMultipleChoiceField(
+    supplied_by = forms.ModelMultipleChoiceField(
                    queryset = Technology.objects.all()
                  , label = 'Supplied By'
                  , widget = admin.widgets.FilteredSelectMultiple('Technologies', False)
                  , required = False
                  )
     ammo_for = forms.ModelChoiceField(
-                   queryset = models.Equipment.objects.filter(has_ammo=True)
-                 , label = 'Ammunition For'
-                 , required = False
-                 )
+                 queryset = models.Equipment.objects.filter(has_ammo=True)
+               , label = 'Ammunition For'
+               , required = False
+               )
+
+    class Meta:
+        model = models.Equipment
+        fields = ('name', 'equipment_class', 'ssw_name', 'tonnage_func','tonnage_factor', 'critical_func', 'critical_factor'
+                 , 'cost_func', 'cost_factor', 'splittable', 'crittable', 'has_ammo', 'ammo_for','ammo_size', 'basic_ammo'
+                 , 'weapon_properties', 'evaluate_last', 'fcs_artemis_iv', 'fcs_artemis_v', 'fcs_apollo', 'supplied_by'
+                 , 'record_status')
+        labels = {
+          'ssw_name' : 'Skunkwerks Name'
+        , 'tonnage_func' : 'Tonnage Formula'
+        , 'tonnage_factor' : 'Tonnage Factor'
+        , 'critical_func' : 'Criticals Formula'
+        , 'critical_factor' : 'Criticals Factor'
+        , 'cost_func' : 'Cost Formula'
+        , 'cost_factor' : 'Cost Factor'
+        , 'fcs_artemis_iv' : 'Supports Artemis IV'
+        , 'fcs_artemis_v' : 'Supports Artemis V'
+        , 'fcs_apollo' : 'Supports Apollo'
+        } 
 
 class HasTechnologyFilter(admin.SimpleListFilter):
     title = 'Has Technology'
@@ -36,4 +55,19 @@ class EquipmentAdmin(admin.ModelAdmin):
     model = models.Equipment
     form = EquipmentForm
     list_filter = ('equipment_class', 'record_status', HasTechnologyFilter)
+
+    readonly_fields = ('ssw_name',)
+    fields = ( 'ssw_name'
+             , ('name', 'equipment_class')
+             , ('tonnage_func','tonnage_factor')
+             , ('critical_func', 'critical_factor')
+             , ('cost_func', 'cost_factor')
+             , ('splittable', 'crittable')
+             , ('has_ammo', 'ammo_for','ammo_size', 'basic_ammo')
+             , 'weapon_properties'
+             , 'evaluate_last'
+             , ('fcs_artemis_iv', 'fcs_artemis_v', 'fcs_apollo')
+             , 'supplied_by'
+             , 'record_status'
+             ) 
             
