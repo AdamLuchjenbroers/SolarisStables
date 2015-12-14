30912c4e142489d03e60a3be03f09ae2c9dba0e9
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2015-12-14 20:30:18 +1100

    Add test-cases for direct-fire tonnage

--- solaris/utilities/test_mechloader.py
+++ solaris/utilities/test_mechloader.py
@@ -10,24 +10,29 @@ class MechTestMixin(object):
     ,   'omni_loadout' : 'Base'
     }
     movement_profile = [5,8,0]
+    directfire_tonnage = None
 
     def setUp(self):
         self.mech = MechDesign.objects.get(**self.__class__.mech_ident)
 
-    def mp_comparison(self, mp, expected, errorText):
-        self.assertEqual(mp, expected, errorText % (mp, expected))
+    def mech_compare(self, actual, expected, errorText):
+        self.assertEqual(actual, expected, errorText % (self.mech.mech_name, self.mech.mech_code, expected, actual))
 
     def test_walkingMP(self):
-        self.mp_comparison(self.mech.move_walk(), self.__class__.movement_profile[0]
-                          , 'Walking MP Failed, expected %i, got %i')
+        self.mech_compare(self.mech.move_walk(), self.__class__.movement_profile[0]
+                          , '%s %s should have a walking MP of %i, got %i')
 
     def test_runningMP(self):
-        self.mp_comparison(self.mech.move_run(), self.__class__.movement_profile[1]
-                          , 'Running MP Failed, expected %i, got %i')
+        self.mech_compare(self.mech.move_run(), self.__class__.movement_profile[1]
+                          , '%s %s should have a running MP of %i, got %i')
         
     def test_jumpMP(self):
-        self.mp_comparison(self.mech.move_jump(), self.__class__.movement_profile[2]
-                          , 'Jumping MP Failed, expected %i, got %i')
+        self.mech_compare(self.mech.move_jump(), self.__class__.movement_profile[2]
+                          , '%s %s should have a jumping MP of %i, got %i')
+    
+    def test_directFireTonnage(self):
+        self.mech_compare(self.mech.directfire_tonnage(), self.__class__.directfire_tonnage
+                         , '%s %s should have %i tons of direct fire weaponry, got %i') 
 
     def assertEquipment(self, location, slots, ssw_name):
         try:
@@ -51,6 +56,7 @@ class Wolverine7DTests(MechTestMixin, TestCase):
     ,   'omni_loadout' : 'Base'
     }
     movement_profile = [5,8,5]
+    directfire_tonnage = 11
 
     def test_hands(self):
         self.assertEquipment('LA','4','Actuator - Hand')
@@ -69,6 +75,7 @@ class Raven4LTests(MechTestMixin, TestCase):
     ,   'omni_loadout' : 'Base'
     }
     movement_profile = [6,9,0]
+    directfire_tonnage = 2
 
 class Dervish8DTests(MechTestMixin, TestCase):
     mech_ident = {
@@ -77,6 +84,7 @@ class Dervish8DTests(MechTestMixin, TestCase):
     ,   'omni_loadout' : 'Base'
     }
     movement_profile = [5,8,5]
+    directfire_tonnage = 4
 
     def test_engine(self):
         self.engine_checkXL(lt_slots='1,2,3', rt_slots='1,2,3')
@@ -95,6 +103,7 @@ class Griffin1NTests(MechTestMixin, TestCase):
     ,   'omni_loadout' : 'Base'
     }
     movement_profile = [5,8,5]
+    directfire_tonnage = 7
 
 class Hatchetman3FTests(MechTestMixin, TestCase):
     mech_ident = {
@@ -103,6 +112,7 @@ class Hatchetman3FTests(MechTestMixin, TestCase):
     ,   'omni_loadout' : 'Base'
     }
     movement_profile = [4,6,4]
+    directfire_tonnage = 14
 
     def test_hatchet(self):
         self.assertEquipment('RA','5,6,7','Equipment - Hatchet')
