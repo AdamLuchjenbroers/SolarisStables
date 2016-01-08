e364d11d2ad3332f45cbb901a51db49c5465fd7c
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2016-01-08 17:59:35 +1100

    Use simple cross-reference table to consolidate different capitalisations of mech names

--- solaris/utilities/forms.py
+++ solaris/utilities/forms.py
@@ -4,13 +4,26 @@
 '''
 
 from django.forms import ModelForm
+
 from solaris.warbook.mech.models import MechDesign, MechDesignLocation
 from solaris.warbook.equipment.models import Mounting, MechEquipment
+
+mech_names_xref = {
+  'BattleMaster' : 'Battlemaster'
+, 'UrbanMech'    : 'Urbanmech'
+}
 
 class MechValidationForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(MechValidationForm,self).__init__(*args, **kwargs)
-        
+       
+    def clean_mech_name(self):
+        old_name = self.cleaned_data.get('mech_name')
+        if old_name in mech_names_xref:
+            return mech_names_xref[old_name]
+        else:
+            return old_name
+ 
     class Meta:
         model = MechDesign
         fields = ( 'mech_name', 'mech_code', 'omni_loadout', 'stock_design', 'credit_value', 'bv_value', 'tonnage'
