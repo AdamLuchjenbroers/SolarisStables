14f77120d61948f202f356f96c060602db303778
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2016-01-22 18:57:31 +1100

    Add list-available-techs ajax view

--- solaris/stablemanager/ajax.py
+++ solaris/stablemanager/ajax.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.views.generic import View
 
 from .views import StableWeekMixin
+from solaris.warbook.techtree.models import Technology
 
 class ProductionChassisAutocomplete(StableWeekMixin, View):
     def get(self, request):  
@@ -22,4 +23,11 @@ class ListProductionVariants(StableWeekMixin, View):
         for mech in self.stableweek.supply_mechs.filter(mech_name=request.GET['mech']):
             variant_list[mech.mech_code] = True      
                  
-        return HttpResponse(json.dumps(variant_list.keys()))  
+        return HttpResponse(json.dumps(variant_list.keys())) 
+
+class ListAvailableTechContracts(StableWeekMixin, View):
+    def get(self, request, week=None):
+        techlist = Technology.objects.exclude(id__in=self.stableweek.supply_contracts.all()).filter(name__icontains=request.GET['term'])
+        technames = [ tech.name for tech in techlist ]
+
+        return HttpResponse(json.dumps(technames)) 
