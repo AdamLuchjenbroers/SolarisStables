c6909249791e0602a524609bdebb65fd8d0ce05c
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2016-02-21 12:28:31 +1100

    Add loading spinner for add-tech action

--- static/js/stable_overview.js
+++ static/js/stable_overview.js
@@ -1,5 +1,4 @@
-
-$( document ).ready(function() {
+function attach_tech_input(form, button, input) {
   $('#add-tech-input').autocomplete({
     source: window.location.href + '/list-techs'
   , minLength: 3
@@ -11,10 +10,23 @@ $( document ).ready(function() {
       url  : window.location.href + '/add-tech',
       dataType : 'json',
       data : {
-        tech : $("#add-tech-input").val()
+        tech : $('#add-tech-input').val()
       },
+      beforeSend: function(jqXHR, plainObj) {
+        $('#add-tech-form .spinner').show();
+        $('#add-tech-form .action').hide();
+      }, 
+      complete: function(jqXJR, plainObj) {
+        $('#add-tech-form .spinner').hide();
+        $('#add-tech-form .action').show();
+      }, 
     }).done(function(newState) {
       $("#stable-tech-list").load(window.location.href + '/tech-list')    
+      attach_tech_input();
     });
   });
-})
+}
+
+$( document ).ready(function() {
+  attach_tech_input('#add-tech-input');
+});
