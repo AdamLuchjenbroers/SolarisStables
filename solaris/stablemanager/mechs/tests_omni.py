07b019f0e97788e45d3a3453dad7d744a58f36e5
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2016-07-08 21:26:51 +1000

    Add omnimech test cases

---
+++ solaris/stablemanager/mechs/tests_omni.py
@@ -0,0 +1,53 @@
+
+from django.test import TestCase
+
+from solaris.stablemanager.tests import StableTestMixin
+
+class OmniMechTests(StableTestMixin, TestCase):
+    def setUp(self):
+        self.stable = self.createStable()
+        self.mech = self.addMech(self.stable, mech_name='Owens', mech_code='OW-1', omni_loadout='C') 
+        self.base_filter = {
+          'current_design__mech_name' : 'Owens'
+        , 'current_design__mech_code' : 'OW-1'
+        , 'current_design__omni_loadout' : 'Base'
+        }
+
+        self.config_filter = {
+          'current_design__mech_name' : 'Owens'
+        , 'current_design__mech_code' : 'OW-1'
+        , 'current_design__omni_loadout' : 'C'
+        }
+
+    def test_baseChassis(self):
+        week = self.stable.get_stableweek(1)
+        smw = week.mechs.get(**self.base_filter)
+        self.assertEquals(smw.config_for, None, 'Config_for should not be populated for base chassis') 
+
+    def test_baseChassisLoadouts(self):
+        week = self.stable.get_stableweek(1)
+        smw = week.mechs.get(**self.base_filter)
+        self.assertEquals(smw.loadouts.count(), 1, 'Base loadout should have 1 available config, found %d' % smw.loadouts.count()) 
+
+    def test_configLoaded(self): 
+        week = self.stable.get_stableweek(1)
+        smw = week.mechs.get(**self.config_filter)
+        base_smw = week.mechs.get(**self.base_filter)
+        self.assertEquals(smw.config_for, base_smw, 'Config_for should refer to the base chassis for the Omnimech')
+
+    def test_nextWeekBaseChassis(self):
+        week = self.advanceWeek(self.stable)
+        smw = week.mechs.get(**self.base_filter)
+        self.assertEquals(smw.config_for, None, 'Config_for should not be populated for base chassis') 
+
+    def test_nextWeekBaseChassisLoadouts(self):
+        week = self.advanceWeek(self.stable)
+        smw = week.mechs.get(**self.base_filter)
+        self.assertEquals(smw.loadouts.count(), 1, 'Base loadout should have 1 available config, found %d' % smw.loadouts.count()) 
+       
+    def test_nextWeekConfigLoaded(self): 
+        week = self.advanceWeek(self.stable)
+        smw = week.mechs.get(**self.config_filter)
+        base_smw = week.mechs.get(**self.base_filter)
+        self.assertEquals(smw.config_for, base_smw, 'Config_for should refer to the base chassis for the Omnimech')
+          
