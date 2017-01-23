43830b1293bd83545656c65553ddc31e15e7da67
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2017-01-23 21:56:01 +1100

    Make Heading 3 a bit bigger

--- solaris/pdf_styles.py
+++ solaris/pdf_styles.py
@@ -4,7 +4,7 @@ body_text = ParagraphStyle('body', fontName='Helvetica', fontSize=10, leading=14
 
 heading_1 = ParagraphStyle('heading1', fontName='Helvetica', fontSize=22, leading=28)
 heading_2 = ParagraphStyle('heading2', fontName='Helvetica', fontSize=18, leading=22)
-heading_3 = ParagraphStyle('heading3', fontName='Helvetica', fontSize=12, leading=14)
+heading_3 = ParagraphStyle('heading3', fontName='Helvetica', fontSize=14, leading=18)
 
 headings = [heading_1, heading_2, heading_3]
 
