c52ceec7c07141cb01fd6aeba8e56e17ecd312ac
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2017-01-23 21:57:34 +1100

    Add mech listing to stable owners report

--- solaris/stablemanager/pdf.py
+++ solaris/stablemanager/pdf.py
@@ -8,6 +8,7 @@ from reportlab.platypus import Table, TableStyle, PageBreak, Paragraph
 from solaris import pdf_styles, pdf
 from solaris.stablemanager.ledger.pdf import LedgerReportSection
 from solaris.stablemanager.pilots.pdf import RosterReportSection
+from solaris.stablemanager.mechs.pdf import MechsReportSection
 
 class StableDocTemplate(SolarisDocTemplate):
     def __init__(self, request, stable=None, stableweek=None, **kwargs):
@@ -64,6 +65,7 @@ class StablePDFReport(StableWeekMixin, PDFView):
         story += OverviewReportSection(self.stableweek).as_story()
         story += LedgerReportSection(self.stableweek, width=(A4[1]*0.8)).as_story()
         story += RosterReportSection(self.stableweek, width=(A4[1]*0.8)).as_story()
+        story += MechsReportSection(self.stableweek).as_story()
 
         self.doc.build(story)
         return self.doc.get_response() 
