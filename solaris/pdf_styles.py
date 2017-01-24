9dcfed773db1d755570853f408940d6e51e5ebb8
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2017-01-24 21:43:13 +1100

    Use Bold Text for headings

--- solaris/pdf_styles.py
+++ solaris/pdf_styles.py
@@ -2,9 +2,9 @@ from reportlab.lib.styles import ParagraphStyle
 
 body_text = ParagraphStyle('body', fontName='Helvetica', fontSize=10, leading=14)
 
-heading_1 = ParagraphStyle('heading1', fontName='Helvetica', fontSize=22, leading=28)
-heading_2 = ParagraphStyle('heading2', fontName='Helvetica', fontSize=18, leading=22)
-heading_3 = ParagraphStyle('heading3', fontName='Helvetica', fontSize=14, leading=18)
+heading_1 = ParagraphStyle('heading1', fontName='Helvetica-Bold', fontSize=22, leading=28)
+heading_2 = ParagraphStyle('heading2', fontName='Helvetica-Bold', fontSize=18, leading=22)
+heading_3 = ParagraphStyle('heading3', fontName='Helvetica-Bold', fontSize=14, leading=18)
 
 headings = [heading_1, heading_2, heading_3]
 
