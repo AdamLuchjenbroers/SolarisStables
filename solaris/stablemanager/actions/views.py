a2d91e25d23dbb20ad5dbeb6c1a12647c5f963e9
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2017-04-07 00:08:07 +1000

    Rename app due to names conflict

--- solaris/stablemanager/actions/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import TemplateView
-
-from solaris.stablemanager.views import StableWeekMixin
-
-class StableActionView(StableWeekMixin, TemplateView):
-    submenu_selected = 'Actions'
-    template_name = 'stablemanager/stable_actions.tmpl'
-    view_url_name = 'stable_actions'
-        
