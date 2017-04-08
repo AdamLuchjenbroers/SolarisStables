144a3dd750acf126292c21d5bd3e393088dc5195
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2017-04-08 19:06:14 +1000

    Add JSON query to get action type data

--- solaris/warbook/actions/urls.py
+++ solaris/warbook/actions/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = patterns('',
     url(r'^/?$', views.ActionGroupListView.as_view(), name='actions'),
     url(r'^(?P<slug>[^/]+)/?$', views.ActionGroupDetailView.as_view(), name='action_group_detail' ),
+
+    url(r'^(?P<pk>[0-9]+)/json', views.ActionDetailJsonView.as_view(), name='action_jsondata')
 )
--- solaris/warbook/actions/views.py
+++ solaris/warbook/actions/views.py
@@ -1,5 +1,9 @@
 
 from django.views.generic import DetailView, ListView
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404, HttpResponse
+
+import json
 
 from solaris.warbook.views import ReferenceViewMixin
 from . import models
@@ -14,3 +18,18 @@ class ActionGroupDetailView(ReferenceViewMixin, DetailView):
     template_name = 'warbook/actiongroupdetail.html'
     slug_field = 'group__iexact'
     model = models.ActionGroup
+
+class ActionDetailJsonView(DetailView):
+    model = models.ActionType
+
+    def get(self, request, *arg, **kwargs):
+        action = self.get_object()
+
+        action_data = {
+          'group'         : action.group.group
+        , 'action'        : action.action
+        , 'description'   : action.description.rendered
+        , 'base_cost'     : action.base_cost
+        }
+
+        return HttpResponse(json.dumps(action_data))
