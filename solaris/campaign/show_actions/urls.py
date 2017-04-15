7610c57a0a643a2435bac0b4a6b0b2c22c58c6c9
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2017-04-15 11:24:20 +1000

    Fix typo - missing as_view()

--- solaris/campaign/show_actions/urls.py
+++ solaris/campaign/show_actions/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     url(r'^(?P<week>[0-9]+)/?$', views.CampaignActionsView.as_view(), name='campaign_actions'),
 
     url(r'^(?P<week>[0-9]+)/list$', views.CampaignActionsListPart.as_view(), name='campaign_actions_listpart'),
-
     url(r'^(?P<week>[0-9]+)/start$', views.AjaxSetWeekStarted.as_view(), name='campaign_actions_start'),
+
+    url(r'^(?P<week>[0-9]+)/(?P<stable>[^/]+)/?$', views.CampaignListStableActions.as_view(), name='campaign_actions_stable'),
 )    
