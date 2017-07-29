ae632f5a822342d5ea099f5defc8e1428e13f08e
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2017-07-29 13:19:17 +1000

    Refactoring - move mech urls into own model section

--- solaris/warbook/urls.py
+++ solaris/warbook/urls.py
@@ -12,11 +12,9 @@ urlpatterns = patterns('',
     url(r'^pilot-skills/?$', DisciplineListView.as_view(), name='pilot_skills' ),
     url(r'^pilot-skills/(?P<slug>[^/]+)/?$', DisciplineDetailView.as_view(), name='pilot_skill_detail' ),
     url(r'^pilot-issues/?$', TraitsDetailView.as_view(), {'slug' : 'pilot-issues' }, name='pilot_issues', ),
-    url(r'^mechs/?$', MechSearchView.as_view(), name='mech_search') ,
-    url(r'^mechs/search/?$', MechSearchResultsView.as_view(), name='mech_searchresult'),
-    url(r'^mechs/(?P<name>[^/]+)/?$', MechListView.as_view(), name='mech_chassis') ,
-    url(r'^mechs/(?P<name>[^/]+)/(?P<code>[^/]+)/(?P<omni>[^/]+)/?$', MechDetailView.as_view(), name='mech_detail') ,
-    url(r'^mechs/(?P<name>[^/]+)/(?P<code>[^/]+)/?$', MechDetailView.as_view(), name='mech_detail_base') ,
+
+    url(r'^mechs/', include('solaris.warbook.mech.urls')),
+
     url(r'^ajax/?', include('solaris.warbook.ajax_urls')),    
     url(r'^actions/', include('solaris.warbook.actions.urls')),
 )
