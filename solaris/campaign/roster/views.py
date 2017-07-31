963d03fc7a64c3e6485eff77723ce59b6a2354f3
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2017-07-31 17:39:43 +1000

    WIP - Fights Roster page

--- solaris/campaign/roster/views.py
+++ solaris/campaign/roster/views.py
@@ -1,8 +1,25 @@
 from django.views.generic import TemplateView, View
 
 from solaris.campaign.views import CampaignWeekMixin
+from solaris.warbook.fightinfo.models import FightGroup
 
-class FightRosterView(CampaignWeekMixin, TemplateView):
+class FightRosterMixin(CampaignWeekMixin):
+    def get_context_data(self, **kwargs):
+        page_context = super(FightRosterMixin, self).get_context_data(**kwargs)
+
+        group_list = []
+
+        for group in FightGroup.objects.all():
+            fights = self.week.fights.filter(fight_type__group=group)
+
+            if fights.count() > 0:
+              group_list.append({'name' : group.name, 'fights' : fights})
+
+        page_context['group_list'] = group_list
+
+        return page_context
+
+class FightRosterView(FightRosterMixin, TemplateView):
     template_name = 'campaign/fights.html'
     view_url_name = 'campaign_fights'
     submenu_selected = 'Fights'
