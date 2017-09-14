d1c5fee422adde3086d9584e0f410caec8cfa938
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2017-09-14 23:50:07 +1000

    Save/Restore stable discipline lists when replacing skills list

--- solaris/warbook/migrations/0021_season3_skillsdata.py
+++ solaris/warbook/migrations/0021_season3_skillsdata.py
@@ -23,10 +23,28 @@ def remove_secondary_training_cost(apps, schema_editor):
 
 def reload_trait_groups(apps, schema_editor):
     PilotTraitGroup = apps.get_model('warbook', 'PilotTraitGroup')
+ 
+    # Stash a list of stable disciplines so we can restore them after reloading the table.
+    Stable = apps.get_model('stablemanager', 'Stable')
+
+    stable_disciplines = dict()
+    for s in Stable.objects.all():
+        discipline_list = (discipline.name for discipline in s.stable_disciplines.all())
+        stable_disciplines[s] = discipline_list
+
+        # Clear the list to avoid primary key issues.
+        s.stable_disciplines.all().delete()
+
     fields = ['name', 'discipline_type', 'rank_restricted', 'urlname' ,'blurb']
 
     PilotTraitGroup.objects.all().delete()
     load_pilottraitgroup_csv('%s/data/warbook.pilottraitgroup.csv' % settings.BASE_DIR, csvfields=fields, PilotTraitGroup=PilotTraitGroup );
+
+    # Restore Disciplines list.
+    for stable, discipline_list in stable_disciplines.items():
+        for discipline in discipline_list:
+            discipline_group = PilotTraitGroup.objects.get(name=discipline)
+            stable.stable_disciplines.add(discipline_group)
  
 def reload_traits(apps, schema_editor):
     PilotTrait = apps.get_model('warbook','PilotTrait')
@@ -45,6 +63,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('warbook', '0020_season3_skills'),
+        ('stablemanager', '0028_asset_count_details'),
     ]
 
     operations = [
