b9a313d40f292b498bb50d7bbcf34aa044c6762e
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2017-10-11 21:40:10 +1100

    Format tweaks for add-fight form

--- solaris/campaign/roster/forms.py
+++ solaris/campaign/roster/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from solaris.warbook.fightinfo.models import fight_list_as_opttree
+from solaris.warbook.mech.models import MechDesign
 
 from . import models
 
@@ -8,9 +9,20 @@ class AddFightForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(AddFightForm, self).__init__(*args, **kwargs)
 
-        self.fields['fight_type'].label = 'Fight Type'
         self.fields['fight_type'].choices = fight_list_as_opttree()
 
+        omnis = MechDesign.objects.filter(is_omni=True, omni_basechassis=None).order_by('mech_name','mech_code')
+        omni_choices = [(None, '---------'), ] +[ (m.id, '%s %s' % (m.mech_name, m.mech_code)) for m in omnis]
+        self.fields['chassis'].choices = omni_choices
+        
     class Meta:
         model = models.RosteredFight
-        fields = ('fight_type', 'fight_map', 'units', 'weightclass', 'tonnage', 'chassis', 'fight_class', 'purse')
+        fields = ('fight_type', 'fight_map', 'group_units', 'weightclass', 'group_tonnage', 'chassis', 'fight_class', 'purse')
+        labels = {
+          'fight_type' : 'Fight Type'
+        , 'fight_map'  : 'Map'
+        , 'chassis'    : 'Omnimech Chassis'
+        , 'group_units' : 'Units'
+        , 'group_tonnage' : 'Tonnage'
+        , 'fight_class' : 'Fight Class (Manual Text)'
+        }
