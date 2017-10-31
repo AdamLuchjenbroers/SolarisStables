9b20523b88d5b130e449068ea21a0cfd4602c075
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2017-11-01 10:10:11 +1100

    Fix typo in URL spec

--- solaris/campaign/roster/urls.py
+++ solaris/campaign/roster/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = patterns('',
     url(r'^/?$', views.FightRosterView.as_view(), name='campaign_fights_now'),
     url(r'^(?P<week>[0-9]+)/?$', views.FightRosterView.as_view(), name='campaign_fights'),
-    url(r'^(?P<week>[0-9]+)/add?$', views.AddFightFormView.as_view(), name='campaign_add_fights'),
+    url(r'^(?P<week>[0-9]+)/add/?$', views.AddFightFormView.as_view(), name='campaign_add_fights'),
 )
