ea7943c7eb9f2f12f167acc519ec9c24cd1747f5
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2018-05-06 00:44:57 +1000

    WIP - Mech Rolling Table Generator

--- solaris/campaign/tools/urls.py
+++ solaris/campaign/tools/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     url(r'^/?$', views.CampaignToolsView.as_view(), name='campaign_tools_now'),
     url(r'^(?P<week>[0-9]+)/?$', views.CampaignToolsView.as_view(), name='campaign_tools'),
 
-    url(r'^/mech-list/generate$', views.MechRollFormView.as_view(), name='campaign_tools_genmechlist'),
-    url(r'^/mech-list?$', views.MechRollTableView.as_view(), name='campaign_tools_mechlist'),
+    url(r'^mech-list/generate$', views.MechRollFormView.as_view(), name='campaign_tools_genmechlist'),
+    url(r'^mech-list/?$', views.MechRollTableView.as_view(), name='campaign_tools_mechlist'),
 )
--- solaris/campaign/tools/views.py
+++ solaris/campaign/tools/views.py
@@ -1,10 +1,21 @@
 from django.views.generic import FormView, TemplateView
 from django.shortcuts import redirect
+from django.http import Http404
 
+from random import shuffle
+
+from solaris.utils import random_unique_set
 from solaris.campaign.views import CampaignViewMixin, CampaignWeekMixin
 from solaris.warbook.mech.views import MechSearchResultsView
 
 from . import forms
+
+tables = {
+  '1D6'  : { 'items' : 6,  'grouping' : [''], 'per-group' : 6 }
+, 'D6-2' : { 'items' : 12, 'grouping' : ['1-3', '4-6'], 'per-group' : 6 }
+, 'D6-3' : { 'items' : 18, 'grouping' : ['1-2', '3-4', '5-6'], 'per-group' : 6 }
+, '1D20' : { 'items' : 20, 'grouping' : [''], 'per-group' : 20 }
+}
 
 class CampaignToolsView(CampaignWeekMixin, TemplateView):
     template_name = 'campaign/campaign_tools.html'
@@ -20,7 +31,35 @@ class MechRollTableView(MechSearchResultsView):
     def get_context_data(self, **kwargs):
         page_context = super(MechRollTableView, self).get_context_data(**kwargs)
 
-class MechRollFormView(MechSearchResultsView):       
+        tableinfo = tables[self.request.POST.get('table_type')]
+
+        mech_list = self.get_queryset()
+        mech_count = mech_list.count()
+        to_get = tableinfo['items']
+        
+        if mech_count <= 0:
+            raise Http404('No Mechs Found')
+
+        fetch = range(mech_count) * (to_get / mech_count)
+        fetch += random_unique_set(to_get % mech_count, 0, mech_count)
+        shuffle(fetch)
+
+        groups = []
+            
+        for label in tableinfo['grouping']:
+            index_list =  [ fetch.pop() for i in range(tableinfo['per-group']) ]
+            group_mechs = [ mech_list[idx] for idx in index_list ]
+
+            groups.append((label, group_mechs))
+
+        page_context['pergroup'] = tableinfo['per-group']
+        page_context['groups'] = groups
+
+        print groups
+
+        return page_context;
+
+class MechRollFormView(CampaignViewMixin, FormView):       
     template_name = 'campaign/forms/generate_mech_table.html'
     form_class = forms.MechRollListForm
     submenu_selected = 'Mechs'
