f17f10afedfed29f0b2a384b762670cd29ff73b7
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2019-05-28 18:05:59 +1000

    Refactoring - Add campaign_url attribute

--- solaris/campaign/migrations/0001_initial.py
+++ solaris/campaign/migrations/0001_initial.py
@@ -15,6 +15,9 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=30)),
+                ('urlname', models.CharField(unique=True, max_length=30)),
+                ('campaign_state', models.CharField(default=b'P', max_length=1, choices=[(b'P', b'Preparation'), (b'A', b'Active'), (b'C', b'Complete')])),
+                ('invite_only', models.BooleanField(default=True)),
             ],
             options={
                 'db_table': 'campaign',
--- solaris/campaign/models.py
+++ solaris/campaign/models.py
@@ -2,17 +2,35 @@ from django.db import models
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
+from django.utils.text import slugify
+
 class CampaignManager(models.Manager):
     def get_current_campaign(self):
         return self.get(name=settings.CURRENT_CAMPAIGN)
 
 class Campaign(models.Model):
     name = models.CharField(max_length=30)
+    urlname = models.CharField(max_length=30, unique=True) 
+
+    campaign_states = (
+      ('P', 'Preparation')
+    , ('A', 'Active')
+    , ('C', 'Complete')
+    )
+    campaign_state = models.CharField(max_length=1, choices=campaign_states, default='P')
+
+    invite_only = models.BooleanField(default=True)
 
     objects = CampaignManager()
 
     def __unicode__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.urlname == None:
+            self.urlname = slugify(unicode(self.name))
+
+        super(FightType, self).save()
 
     class Meta:
         verbose_name = 'Campaign'
